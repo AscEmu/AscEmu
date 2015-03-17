ef7b00dbd31b76c7d87e730cb9c32ce81fc2de63
Zyres <mr@eichbuehlerhof.de>
2015-03-17 17:05:10 +0100

    UPDATE: GossipScript for WymrestTemple.

--- src/scripts/GossipScripts/Gossip_WyrmrestTemple.cpp
+++ src/scripts/GossipScripts/Gossip_WyrmrestTemple.cpp
@@ -1,7 +1,7 @@
 /**
  * AscEmu Framework based on ArcEmu MMORPG Server
  * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
- * Copyright (C) 2008-2009 FrozenThrone Shard <http://www.dowlee.it/ft>
+ * Copyright (C) 2008-2009 FrozenThrone Shard
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -19,56 +19,74 @@
 
 #include "Setup.h"
 
-//    Wyrmrest Temple Flighter
-// Top to Bottom & Top to Middle
-#define CN_TOP 26949
-#define NPCTEXT_TOP 12714
-// Middle to Top & Middle to Bottom
-#define CN_MIDDLE 27575
-#define NPCTEXT_MIDDLE 12887
-// Bottom to Top & Bottom to Middle
-#define CN_BOTTOM 26443
-#define NPCTEXT_BOTTOM 12713
+enum CreatureEntry
+{
+    CN_TORASTRASZA      = 26949,
+    CN_AFRASASTRASZ     = 27575,
+    CN_TARIOLSTRASZ     = 26443
+};
 
-class SCRIPT_DECL WyrmrestTemple_FlightGossip : public Arcemu::Gossip::Script
+enum eGossipText
+{
+    GT_TORASTRASZA      = 12714,
+    GT_AFRASASTRASZ     = 12887,
+    GT_TARIOLSTRASZ     = 12713
+};
+
+enum eGossipItems
+{
+    GI_WYMREST_TOP_TO_BOTTOM        = 413,  // "Yes, please, I would like to return to the ground level of the temple."
+    GI_WYMREST_TOP_TO_MIDDLE        = 414,  // "I would like to go Lord Afrasastrasz in the middle of the temple."
+    GI_WYMREST_MIDDLE_TO_TOP        = 415,  // "My lord, I need to get to the top of the temple."
+    GI_WYMREST_MIDDLE_TO_BOTTOM     = 416,  // "Can I get a ride back to ground level, Lord Afrasastrasz?"
+    GI_WYMREST_BOTTOM_TO_TOP        = 417,  // "Steward, please allow me to ride one of the drakes to the queen's chamber at the top of the temple."
+    GI_WYMREST_BOTTOM_TO_MIDDLE     = 418   // "Can you spare a drake to take me to Lord Afrasastrasz in the middle of the temple?"
+};
+
+class WyrmrestTemple_Gossip : public GossipScript
 {
     public:
-        void OnHello(Object* pObject, Player* plr)
+        
+        void GossipHello(Object* pObject, Player* plr)
         {
-            Arcemu::Gossip::Menu menu(pObject->GetGUID(), 0);
+            GossipMenu* Menu;
             switch(pObject->GetEntry())
             {
-                case CN_TOP:
+                case CN_TORASTRASZA:
                     {
-                        menu.setTextID(NPCTEXT_TOP);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_TOP_TO_BOTTOM), 1);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_TOP_TO_MIDDLE), 2);
+                        objmgr.CreateGossipMenuForPlayer(&Menu, pObject->GetGUID(), GT_TORASTRASZA, plr);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_TOP_TO_BOTTOM), 1);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_TOP_TO_MIDDLE), 2);
+                        Menu->SendTo(plr);
                     }
                     break;
-                case CN_MIDDLE:
+                case CN_AFRASASTRASZ:
                     {
-                        menu.setTextID(NPCTEXT_MIDDLE);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_MIDDLE_TO_TOP), 3);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_MIDDLE_TO_BOTTOM), 4);
+                        objmgr.CreateGossipMenuForPlayer(&Menu, pObject->GetGUID(), GT_AFRASASTRASZ, plr);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_MIDDLE_TO_TOP), 3);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_MIDDLE_TO_BOTTOM), 4);
+                        Menu->SendTo(plr);
                     }
                     break;
-                case CN_BOTTOM:
+                case CN_TARIOLSTRASZ:
                     {
-                        menu.setTextID(NPCTEXT_BOTTOM);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_BOTTOM_TO_TOP), 5);
-                        menu.AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_BOTTOM_TO_MIDDLE), 6);
+                        objmgr.CreateGossipMenuForPlayer(&Menu, pObject->GetGUID(), GT_TARIOLSTRASZ, plr);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_BOTTOM_TO_TOP), 5);
+                        Menu->AddItem(ICON_CHAT, plr->GetSession()->LocalizedGossipOption(GI_WYMREST_BOTTOM_TO_MIDDLE), 6);
+                        Menu->SendTo(plr);
                     }
                     break;
             }
 
-            menu.Send(plr);
-        }
+        };
 
-        void OnSelectOption(Object* pObject, Player* plr, uint32 Id, const char* Code)
+        void GossipSelectOption(Object* pObject, Player* plr, uint32 Id, uint32 IntId, const char* Code)
         {
-            Arcemu::Gossip::Menu::Complete(plr);
-            switch(Id)
+            switch(IntId)
             {
+                case 0:
+                    GossipHello(pObject, plr);
+                    break;
                 case 1:
                     plr->TaxiStart(sTaxiMgr.GetTaxiPath(879), 6371, 0);     // Drake
                     break;
@@ -90,15 +108,13 @@ class SCRIPT_DECL WyrmrestTemple_FlightGossip : public Arcemu::Gossip::Script
                 default:
                     break;
             }
-        }
-
+        };
 };
+
 
 void SetupWyrmrestTempleGossip(ScriptMgr* mgr)
 {
-    Arcemu::Gossip::Script* WyrmrestTempleFlightGossip = new WyrmrestTemple_FlightGossip;
-
-    mgr->register_creature_gossip(CN_TOP, WyrmrestTempleFlightGossip);    // Torastrasza <Majordomo to the Ruling Council>
-    mgr->register_creature_gossip(CN_MIDDLE, WyrmrestTempleFlightGossip);    // Lord Afrasastrasz <Commander of Wyrmrest Temple Defenses>
-    mgr->register_creature_gossip(CN_BOTTOM, WyrmrestTempleFlightGossip);    // Tariolstrasz <Steward of Wyrmrest Temple>
+    mgr->register_gossip_script(CN_TORASTRASZA, new WyrmrestTemple_Gossip);
+    mgr->register_gossip_script(CN_AFRASASTRASZ, new WyrmrestTemple_Gossip);
+    mgr->register_gossip_script(CN_TARIOLSTRASZ, new WyrmrestTemple_Gossip);
 }
