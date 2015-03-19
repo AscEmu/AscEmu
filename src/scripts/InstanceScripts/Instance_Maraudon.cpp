317a4775fa7956982bcf453ac631bbf55d6a36e1
DankoDJ <matzesper@aol.com>
2015-03-19 10:33:45 +0100

    FIXED: file include failed (seems someone was really tired ;-) )

--- src/scripts/InstanceScripts/Instance_Maraudon.cpp
+++ src/scripts/InstanceScripts/Instance_Maraudon.cpp
@@ -21,7 +21,7 @@
 
 // \todo move most defines to enum, text to db (use SendScriptTextChatMessage(ID))
 #include "Setup.h"
-#include "Instance_Mauradon.h"
+#include "Instance_Maraudon.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //Maraudon
