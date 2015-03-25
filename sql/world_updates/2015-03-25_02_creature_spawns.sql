7a269959b9fc200e6ea8a7535f3d2ba7db163907
Zyres <mr@eichbuehlerhof.de>
2015-03-25 17:31:24 +0100

    UPDATE: Move creature_spawns in the correct phase (2). This is needed since areaspells working. Ref: #89

---
+++ sql/world_updates/2015-03-25_02_creature_spawns.sql
@@ -0,0 +1,12 @@
+/*
+********************************************************************
+Updates for creature_spawns (misc quests)
+********************************************************************
+*/
+
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 25521;
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 25522;
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 26449;
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 25450;
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 25451;
+UPDATE `creature_spawns` SET `phase` = 2 WHERE `entry` = 26452;
