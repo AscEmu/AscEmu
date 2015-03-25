7f5eb496af91ea634f96a053ce2a4b7a75726166
Zyres <mr@eichbuehlerhof.de>
2015-03-25 20:19:12 +0100

    ADDED: Spawn for quest see: #89

---
+++ sql/world_updates/2015-03-25_03_misc_quest.sql
@@ -0,0 +1,11 @@
+/*
+********************************************************************
+Updates for creature_spawns (misc quests)
+********************************************************************
+*/
+
+INSERT INTO creature_spawns
+   (`id`, `entry`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `movetype`, `displayid`, `faction`, `flags`, `bytes0`, `bytes1`, `bytes2`, `emote_state`, `npc_respawn_link`, `channel_spell`, `channel_target_sqlid`, `channel_target_sqlid_creature`, `standstate`, `death_state`, `mountdisplayid`, `slot1item`, `slot2item`, `slot3item`, `CanFly`, `phase`)
+VALUES
+   (199005, 26225, 571, 3602.77, 3655.36, 35.94, 2.55, 0, 10553, 1975, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+   
