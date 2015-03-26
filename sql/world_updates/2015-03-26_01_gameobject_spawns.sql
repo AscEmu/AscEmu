6aaa342092763a774737f42b4d5d222943fcc02b
Zyres <mr@eichbuehlerhof.de>
2015-03-26 22:49:24 +0100

    FIXED: Questfunktion for 11788 closes #90

---
+++ sql/world_updates/2015-03-26_01_gameobject_spawns.sql
@@ -0,0 +1,11 @@
+/*
+********************************************************************
+Updates for creature_spawns
+********************************************************************
+*/
+
+UPDATE `gameobject_spawns` SET `Flags` = 0 WHERE `id` = 53120;
+
+UPDATE `creature_proto` SET `minlevel` = 71, `maxlevel` = 71, `faction` = 16, `minhealth` = 9291, `maxhealth` = 9291, `mindamage` = 299, `maxdamage` = 426, `armor` = 7018  WHERE `entry` = 25831;
+UPDATE `creature_proto` SET `minlevel` = 71, `maxlevel` = 71, `faction` = 16, `minhealth` = 9291, `maxhealth` = 9291, `mindamage` = 299, `maxdamage` = 426, `armor` = 7018  WHERE `entry` = 25832;
+UPDATE `creature_proto` SET `minlevel` = 71, `maxlevel` = 71, `faction` = 16, `minhealth` = 9291, `maxhealth` = 9291, `mindamage` = 299, `maxdamage` = 426, `armor` = 7018  WHERE `entry` = 25833;
