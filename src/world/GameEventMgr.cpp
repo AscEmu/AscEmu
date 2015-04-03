30e458f568956bf4938ced8fd7a7936f7f917b1c
Zyres <mr@eichbuehlerhof.de>
2015-04-03 21:12:10 +0200

    REMOVED: "for each" was completely wrong, just use "for". Now we didnt need a special compiler definition. :-) Thanks @Evairfairy for verify

--- src/world/GameEventMgr.cpp
+++ src/world/GameEventMgr.cpp
@@ -287,11 +287,8 @@ void GameEventMgr::GameEventMgrThread::Update()
 {
     //Log.Notice("GameEventMgr", "Tick!");
     auto now = time(0);
-    #if COMPILER == COMPILER_MICROSOFT
-    for each (auto gameEventPair in sGameEventMgr.mGameEvents)
-    #else
+
     for (auto gameEventPair : sGameEventMgr.mGameEvents)
-    #endif
     {
         GameEvent* gameEvent = gameEventPair.second;
 
