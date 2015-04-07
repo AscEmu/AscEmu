93e0819a5d4cffedaae116c0d4228ba5f3174742
Zyres <mr@eichbuehlerhof.de>
2015-04-07 10:07:29 +0200

    UPDATE: Gossip text for ICC and Ulduar teleporter. Correction for gossip items.

--- src/world/Master.cpp
+++ src/world/Master.cpp
@@ -573,7 +573,7 @@ bool Master::Run(int argc, char** argv)
 }
 
 static const char *REQUIRED_CHAR_DB_VERSION = "2015-03-21_01_mailbox";
-static const char *REQUIRED_WORLD_DB_VERSION = "2015-04-06_03_creature_spawns";
+static const char *REQUIRED_WORLD_DB_VERSION = "2015-04-07_01_npc_text";
 
 bool Master::CheckDBVersion()
 {
