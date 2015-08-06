ba9f676918bcf1104a41c1a53b8db38db67b0873
Zyres <mr@eichbuehlerhof.de>
2015-08-06 03:55:14 +0200

    I forgot there are a few people using encrypted_password alread (instead of password).

--- sql/logon_updates/2015-08-06_01_accounts_password.sql
+++ sql/logon_updates/2015-08-06_01_accounts_password.sql
@@ -5,7 +5,7 @@ Updates for accounts
 */
 
 -- Set all accounts to encrypted password
-UPDATE accounts SET encrypted_password = SHA(CONCAT(UPPER(login),":",UPPER(password)));
+UPDATE accounts SET encrypted_password = SHA(CONCAT(UPPER(login),":",UPPER(password))) WHERE password <> '';
 
 -- Delete cleartext column password
 ALTER TABLE accounts DROP COLUMN password;
