af221ce7603fa2d0d724c6343077234bde699cb6
DankoDJ <matzesper@aol.com>
2015-09-15 17:36:50 +0200

    Added files for Utargde Pinnacle. Changed wrong copyright in VioletHold header file (this file was never part of Sun++ scripts).

--- src/scripts/InstanceScripts/Instance_UtgardePinnacle.cpp
+++ src/scripts/InstanceScripts/Instance_UtgardePinnacle.cpp
@@ -1,9 +1,9 @@
 /*
- * AscEmu Framework based on Arcemu MMORPG Server
+ * AscEmu Framework based on ArcEmu MMORPG Server
  * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
@@ -12,26 +12,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _INSTANCE_THE_VIOLET_HOLD_H
-#define _INSTANCE_THE_VIOLET_HOLD_H
-
-enum CreatureEntry
-{
-
-};
-
-enum CreatureSpells
-{
-
-};
-
-enum CreatureSay
-{
-
-};
-
-#endif // _INSTANCE_THE_VIOLET_HOLD_H
+#include "Setup.h"
+#include "Instance_UtgardePinnacle.h"
--- src/scripts/InstanceScripts/Instance_UtgardePinnacle.h
+++ src/scripts/InstanceScripts/Instance_UtgardePinnacle.h
@@ -1,9 +1,9 @@
 /*
- * AscEmu Framework based on Arcemu MMORPG Server
+ * AscEmu Framework based on ArcEmu MMORPG Server
  * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
@@ -12,12 +12,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _INSTANCE_THE_VIOLET_HOLD_H
-#define _INSTANCE_THE_VIOLET_HOLD_H
+#ifndef _INSTANCE_UTGARDE_PINNACLE_H
+#define _INSTANCE_UTGARDE_PINNACLE_H
 
 enum CreatureEntry
 {
@@ -34,4 +34,4 @@ enum CreatureSay
 
 };
 
-#endif // _INSTANCE_THE_VIOLET_HOLD_H
+#endif // _INSTANCE_UTGARDE_PINNACLE_H
