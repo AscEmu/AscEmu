ab0122147fbe646048e1d740afa02920d287311f
Zyres <mr@eichbuehlerhof.de>
2015-09-17 17:39:44 +0200

    Updated characters table use tinyint(1) for a boolean. Changed file 2015-09-14_01_rbg_daily.sql (see MySQL standards on our wiki)

--- src/world/Master.cpp
+++ src/world/Master.cpp
@@ -572,7 +572,7 @@ bool Master::Run(int argc, char** argv)
     return true;
 }
 
-static const char *REQUIRED_CHAR_DB_VERSION = "2015-09-14_01_rbg_daily";
+static const char *REQUIRED_CHAR_DB_VERSION = "2015-09-17_01_characters";
 static const char *REQUIRED_WORLD_DB_VERSION = "2015-09-07_01_gameobject_names";
 
 bool Master::CheckDBVersion()
