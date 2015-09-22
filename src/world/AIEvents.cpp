a2a06312fa5e91b9cd03dc6799d5125aa92f9d02
Rachael Hepworth <evairfairy@gmail.com>
2015-09-22 16:02:47 +0100

    More missing headers

--- src/world/AIEvents.cpp
+++ src/world/AIEvents.cpp
@@ -18,6 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Unit.h"
+#include "AIInterface.h"
 #include "StdAfx.h"
 
 pAIEvent AIEventHandlers[NUM_AI_EVENTS] =
