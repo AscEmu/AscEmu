9262f651eb697bd422f969b669a3a8d476ded9ed
Zyres <mr@eichbuehlerhof.de>
2015-09-27 17:26:20 +0200

    Updated g3dlite to a newer version.

--- dep/g3dlite/debugAssert.cpp
+++ dep/g3dlite/debugAssert.cpp
@@ -11,7 +11,7 @@
 
 #include "G3D/debugAssert.h"
 #include "G3D/platform.h"
-#ifdef G3D_WIN32
+#ifdef G3D_WINDOWS
     #include <tchar.h>
 #endif
 #include "G3D/format.h"
@@ -28,6 +28,8 @@
 #   pragma warning (disable : 4530)
 #endif
 
+using namespace std;
+
 namespace G3D { namespace _internal {
 
 ConsolePrintHook _consolePrintHook;
@@ -35,14 +37,14 @@ AssertionHook _debugHook = _handleDebugAssert_;
 AssertionHook _failureHook = _handleErrorCheck_;
 
 #ifdef G3D_LINUX
-#if SOMEONE_MADE_THIS_USEFUL
+#if 0 /* G3DFIX: Disabled to avoid requirement for X11 libraries */
     Display*      x11Display = NULL;
     Window        x11Window  = 0;
 #endif
 #endif
 
 
-#ifdef G3D_WIN32
+#ifdef G3D_WINDOWS
 static void postToClipboard(const char *text) {
     if (OpenClipboard(NULL)) {
         HGLOBAL hMem = GlobalAlloc(GHND | GMEM_DDESHARE, strlen(text) + 1);
@@ -75,7 +77,7 @@ static void createErrorMessage(
     std::string le = "";
     const char* newline = "\n";
 
-    #ifdef G3D_WIN32
+    #ifdef G3D_WINDOWS
         newline = "\r\n";
 
         // The last error value.  (Which is preserved across the call).
@@ -104,9 +106,9 @@ static void createErrorMessage(
             realLastErr = _T("Last error code does not exist.");
         }
 
-		if (lastErr != 0) {
-	        le = G3D::format("Last Error (0x%08X): %s\r\n\r\n", lastErr, (LPCSTR)realLastErr);
-		}
+        if (lastErr != 0) {
+            le = G3D::format("Last Error (0x%08X): %s\r\n\r\n", lastErr, (LPCSTR)realLastErr);
+        }
 
         // Get rid of the allocated memory from FormatMessage.
         if (NULL != formatMsg) {
@@ -117,8 +119,10 @@ static void createErrorMessage(
         GetModuleFileNameA(NULL, modulePath, MAX_PATH);
 
         const char* moduleName = strrchr(modulePath, '\\');
-        outTitle = outTitle + std::string(" - ") + std::string(moduleName ? (moduleName + 1) : modulePath);
+        outTitle = outTitle + string(" - ") + string(moduleName ? (moduleName + 1) : modulePath);
 
+    #else
+        (void)outTitle;
     #endif
 
     // Build the message.
@@ -140,7 +144,7 @@ bool _handleDebugAssert_(
     std::string dialogText = "";
     createErrorMessage(expression, message, filename, lineNumber, dialogTitle, dialogText);
 
-    #ifdef G3D_WIN32
+    #ifdef G3D_WINDOWS
         DWORD lastErr = GetLastError();
         postToClipboard(dialogText.c_str());
         debugPrintf("\n%s\n", dialogText.c_str());
@@ -157,7 +161,7 @@ bool _handleDebugAssert_(
 
     int result = G3D::prompt(dialogTitle.c_str(), dialogText.c_str(), (const char**)choices, 3, useGuiPrompt);
 
-#    ifdef G3D_WIN32
+#    ifdef G3D_WINDOWS
         // Put the incoming last error back.
         SetLastError(lastErr);
 #    endif
@@ -198,7 +202,7 @@ bool _handleErrorCheck_(
 
     // Log the error
     Log::common()->print(std::string("\n**************************\n\n") + dialogTitle + "\n" + dialogText);
-    #ifdef G3D_WIN32
+    #ifdef G3D_WINDOWS
         DWORD lastErr = GetLastError();
         (void)lastErr;
         postToClipboard(dialogText.c_str());
@@ -210,7 +214,7 @@ bool _handleErrorCheck_(
     const std::string& m = 
         std::string("An internal error has occured in this program and it will now close.  "
         "The specific error is below. More information has been saved in \"") +
-            Log::getCommonLogFilename() + "\".\n" + dialogText;
+            Log::getCommonLogFilename() + "\".\n\n" + dialogText;
 
     int result = G3D::prompt("Error", m.c_str(), (const char**)choices, 1, useGuiPrompt);
     (void)result;
@@ -219,7 +223,7 @@ bool _handleErrorCheck_(
 }
 
 
-#ifdef G3D_WIN32
+#ifdef G3D_WINDOWS
 static HCURSOR oldCursor;
 static RECT    oldCursorRect;
 static POINT   oldCursorPos;
@@ -227,7 +231,7 @@ static int     oldShowCursorCount;
 #endif
 
 void _releaseInputGrab_() {
-    #ifdef G3D_WIN32
+    #ifdef G3D_WINDOWS
 
         GetCursorPos(&oldCursorPos);
 
@@ -250,13 +254,14 @@ void _releaseInputGrab_() {
         ClipCursor(NULL);
         
     #elif defined(G3D_LINUX)
-#if SOMEONE_MADE_THIS_USEFUL
+#if 0 /* G3DFIX: Disabled to avoid requirement for X11 libraries */
         if (x11Display != NULL) {
             XUngrabPointer(x11Display, CurrentTime);
             XUngrabKeyboard(x11Display, CurrentTime);
             if (x11Window != 0) {
                 //XUndefineCursor(x11Display, x11Window);
-                ///\todo Note that we leak this cursor; it should be freed in the restore code.
+                // TODO: Note that we leak this cursor; it should be
+                // freed in the restore code.
                 Cursor c = XCreateFontCursor(x11Display, 68);
                 XDefineCursor(x11Display, x11Window, c);
             }
@@ -266,13 +271,13 @@ void _releaseInputGrab_() {
         }
 #endif
     #elif defined(G3D_OSX)
-    ///\todo OS X
+        // TODO: OS X
     #endif
 }
 
 
 void _restoreInputGrab_() {
-    #ifdef G3D_WIN32
+    #ifdef G3D_WINDOWS
 
         // Restore the old clipping region
         ClipCursor(&oldCursorRect);
@@ -290,9 +295,9 @@ void _restoreInputGrab_() {
         }
         
     #elif defined(G3D_LINUX)
-    ///\todo Linux
+        // TODO: Linux
     #elif defined(G3D_OSX)
-    ///\todo OS X
+        // TODO: OS X
     #endif
 }
 
@@ -304,7 +309,7 @@ void setAssertionHook(AssertionHook hook) {
 }
 
 AssertionHook assertionHook() {
-    return 	G3D::_internal::_debugHook;
+    return     G3D::_internal::_debugHook;
 }
 
 void setFailureHook(AssertionHook hook) {
@@ -326,7 +331,7 @@ ConsolePrintHook consolePrintHook() {
 
 
 std::string __cdecl debugPrint(const std::string& s) {
-#   ifdef G3D_WIN32
+#   ifdef G3D_WINDOWS
         const int MAX_STRING_LEN = 1024;
     
         // Windows can't handle really long strings sent to
