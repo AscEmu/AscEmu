0b2a2752f838c7b4bdb4965787050aed0cbf5a1a
Zyres <mr@eichbuehlerhof.de>
2015-09-28 18:12:38 +0200

    Moved creature_data (extractor) to tools/extras

--- cmake/AscConfigs.cmake
+++ cmake/AscConfigs.cmake
@@ -10,4 +10,7 @@ endif()
 set(ASCEMU_SCRIPTLIB_PATH "modules" CACHE PATH "The directory for AscEmu modules.")
 set(BUILD_ASCEMUSCRIPTS TRUE CACHE BOOL "Build AscEmu modules.")
 set(BUILD_TOOLS TRUE CACHE BOOL "Build AscEmu tools.")
+set(BUILD_EXTRAS FALSE CACHE BOOL "Build AscEmu extra.")
+
+#Compiler heap limit.
 set(VISUALSTUDIO_COMPILERHEAPLIMIT 400 CACHE STRING "Visual Studio compiler heap limit. Ignore on darwin and unix platforms.")
