371ae0dd3ac3dd334bc9d6d5d88eee2a52185434
DankoDJ <matzesper@aol.com>
2015-09-30 14:14:01 +0200

    Added new file for logon/realm communication.

---
+++ src/logonserver/Opcodes/LogonRealmOpcodes.hpp
@@ -0,0 +1,36 @@
+/*
+ * AscEmu Framework based on ArcEmu MMORPG Server
+ * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LOGON_REALM_OPCODES_H
+#define _LOGON_REALM_OPCODES_H
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/// \brief Logon Realm communication
+///
+/// These opcodes are used for logon/realm communication. All opcodes used by realm is marked
+/// as C (client). All opcodes used by logon is marked as S (server). Our logonserver is
+/// our communication server, the realm is our communication client.
+///
+/// \note work in progress.
+//////////////////////////////////////////////////////////////////////////////////////////
+enum LogonRealmOpcodes
+{
+
+};
+
+#endif      //_LOGON_REALM_OPCODES_H
