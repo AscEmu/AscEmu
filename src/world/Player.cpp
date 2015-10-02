09cf3fea04b73834009ee8b2dfab4f72fa6cc3d2
Zyres <mr@eichbuehlerhof.de>
2015-10-02 13:40:21 +0200

    Resolved CID52962 (Resource leak)

--- src/world/Player.cpp
+++ src/world/Player.cpp
@@ -4098,6 +4098,7 @@ void Player::_ApplyItemMods(Item* item, int16 slot, bool apply, bool justdrokedo
                         m_itemsets.erase(i);
                 }
             }
+            delete Set;
         }
     }
 
