bf4ee3053dc485f3b7325613d3de67be94c35475
Zyres <mr@eichbuehlerhof.de>
2015-10-27 17:08:02 +0100

    Added missing spawns/waypoints in Scourgeholme Moved Lich King spawn to its own phase (should be updated if it is clear which is the correct phase) Close #167

--- src/world/Master.cpp
+++ src/world/Master.cpp
@@ -575,7 +575,7 @@ bool Master::Run(int argc, char** argv)
 }
 
 static const char *REQUIRED_CHAR_DB_VERSION = "2015-09-17_01_characters";
-static const char *REQUIRED_WORLD_DB_VERSION = "2015-10-27_01_misc_spawns";
+static const char *REQUIRED_WORLD_DB_VERSION = "2015-10-27_02_misc_spawns";
 
 bool Master::CheckDBVersion()
 {
