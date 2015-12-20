abf99ae2039129919a197a9e62a09062aad738f7
Zyres <mr@eichbuehlerhof.de>
2015-12-20 21:46:14 +0100

    Do not use double spaces after the WHERE statement. (It makes no difference, just a sytle change)

--- sql/world_updates/2015-12-20_01_misc_update.sql
+++ sql/world_updates/2015-12-20_01_misc_update.sql
@@ -1,20 +1,20 @@
 --
 -- Incorrect entry  for Big Will
 --
-DELETE FROM `creature_names` WHERE  `entry`=100000;
+DELETE FROM `creature_names` WHERE `entry`=100000;
 
 --
 -- Defense System is part of the scripted dungeon.
 --
-DELETE FROM `creature_spawns` WHERE  `id`=199003;
+DELETE FROM `creature_spawns` WHERE `id`=199003;
 
 --
 -- Update spawn identifiers in the database by the provisions of the forum.
 --
-UPDATE `creature_spawns` SET `id`=145455 WHERE  `id`=199000;
-UPDATE `creature_spawns` SET `id`=145456, `entry`=6238 WHERE  `id`=199001;
-UPDATE `creature_spawns` SET `id`=145457 WHERE  `id`=199002;
-UPDATE `creature_spawns` SET `id`=145458 WHERE  `id`=199005;
+UPDATE `creature_spawns` SET `id`=145455 WHERE `id`=199000;
+UPDATE `creature_spawns` SET `id`=145456, `entry`=6238 WHERE `id`=199001;
+UPDATE `creature_spawns` SET `id`=145457 WHERE `id`=199002;
+UPDATE `creature_spawns` SET `id`=145458 WHERE `id`=199005;
 
 --
 -- Update world_db_version
