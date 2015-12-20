7cbbf1f1e34ffabc7c7cc6327e995656fa57322f
Zyres <mr@eichbuehlerhof.de>
2015-12-20 21:25:52 +0100

    Resolved msvc C4302 (PVOID to unsigned long conversion)

--- src/shared/CrashHandler.cpp
+++ src/shared/CrashHandler.cpp
@@ -202,7 +202,7 @@ void PrintCrashInformation(PEXCEPTION_POINTERS except)
     echo("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
     echo("Server has crashed. Reason was:\n");
     echo("   %s at 0x%08X\n", GetExceptionDescription(except->ExceptionRecord->ExceptionCode),
-         (unsigned long)except->ExceptionRecord->ExceptionAddress);
+         except->ExceptionRecord->ExceptionAddress);
 #ifdef REPACK
     echo("%s repack by %s has crashed. Visit %s for support.", REPACK, REPACK_AUTHOR, REPACK_WEBSITE);
 #endif
