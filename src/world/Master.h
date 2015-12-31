8d31dcc0559024203c9cf9070e84c6d749bc4a1b
armm77 <amr.morales@gmail.com>
2015-12-31 12:33:33 -0430

    Update creature_names and creature_proto https://github.com/AscEmu/AscEmu/issues/204

--- src/world/Master.h
+++ src/world/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2015-12-27_01_lag_reports";
-static const char* REQUIRED_WORLD_DB_VERSION = "2015-12-20_02_gameobject_teleport";
+static const char* REQUIRED_WORLD_DB_VERSION = "2015-12-31_01_creature_update";
 
 class Master : public Singleton<Master>
 {
