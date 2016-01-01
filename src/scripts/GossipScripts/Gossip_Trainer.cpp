7fc4335889e5285471dac53794dce6189de1c362
Zyres <mr@eichbuehlerhof.de>
2016-01-01 23:51:16 +0100

    Update copyright note for 2016

--- src/scripts/GossipScripts/Gossip_Trainer.cpp
+++ src/scripts/GossipScripts/Gossip_Trainer.cpp
@@ -1,6 +1,6 @@
 /**
  * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org>
+ * Copyright (C) 2014-2016 AscEmu Team <http://www.ascemu.org>
  * Copyright (C) 2005-2007 Ascent Team
  * Copyright (C) 2007-2015 Moon++ Team <http://www.moonplusplus.info/>
  *
