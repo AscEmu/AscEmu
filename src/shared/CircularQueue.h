4ac43d37892b05b35596fbd321e7cae935162d19
Zyres <mr@eichbuehlerhof.de>
2016-01-12 09:54:16 +0100

    Copyright update, additional changes for https://github.com/AscEmu/AscEmu/commit/7fc4335889e5285471dac53794dce6189de1c362

--- src/shared/CircularQueue.h
+++ src/shared/CircularQueue.h
@@ -1,6 +1,6 @@
 /*
  * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2014-2015 AscEmu Team <http://www.ascemu.org/>
+ * Copyright (C) 2014-2016 AscEmu Team <http://www.ascemu.org/>
  * Copyright (C) 2005-2007 Burlex <burlex@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
