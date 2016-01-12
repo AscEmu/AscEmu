03e514d1dc4a5450a097b6f25b123fef7dd0916d
Zyres <mr@eichbuehlerhof.de>
2016-01-12 09:59:36 +0100

    Removed MySQL related options from ascemuConfig.h since there is no other database supported.

--- src/shared/Database/CreateInterface.cpp
+++ src/shared/Database/CreateInterface.cpp
@@ -20,22 +20,16 @@
 
 #include "DatabaseEnv.h"
 #include "../Log.h"
-
-#if defined(ENABLE_DATABASE_MYSQL)
 #include "MySQLDatabase.h"
-#endif
+
 
 void Database::CleanupLibs()
 {
-#if defined(ENABLE_DATABASE_MYSQL)
     mysql_library_end();
-#endif
 }
 
 
 Database* Database::CreateDatabaseInterface()
 {
-#if defined(ENABLE_DATABASE_MYSQL)
     return new MySQLDatabase();
-#endif
 }
