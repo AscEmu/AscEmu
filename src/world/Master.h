9296d261ac20d08b6eef6d3230d6bf87cb0cf31c
Zyres <mr@eichbuehlerhof.de>
2016-01-19 23:52:09 +0100

    Updated quest `Massacre At Light's Point` set the required mob count to 100 (blizzlike). NOTE you have to delete your cache to see the correct req mob count.

--- src/world/Master.h
+++ src/world/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2015-12-27_01_lag_reports";
-static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-13_01_creature_proto_difficulty";
+static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-19_01_quests";
 
 class Master : public Singleton<Master>
 {
