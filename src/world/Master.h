0780e22ae73ad7995e9565ec61e8bfefe012e907
Zyres <mr@eichbuehlerhof.de>
2016-01-25 11:10:56 +0100

    Added creature loot for item 47241 (Emblem of Triumph). Close #253 @Lectro we should handle the daily limit later.
    Thanks to tdb.

--- src/world/Master.h
+++ src/world/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2015-12-27_01_lag_reports";
-static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-25_01_loot_creatures";
+static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-25_02_loot_creatures";
 
 class Master : public Singleton<Master>
 {
