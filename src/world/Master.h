1cb712982ed2769a76d4fdac2d8be74378d8f68d
Zyres <mr@eichbuehlerhof.de>
2016-02-07 02:21:33 +0100

    Updated/Changed IceCrownCitadel bosses. Removed obsolet creature_entry (difficulty creatures was duplicated in names/proto tables). Changed some factions for these bosses.

--- src/world/Master.h
+++ src/world/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2015-12-27_01_lag_reports";
-static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-06_03_creature_difficulty";
+static const char* REQUIRED_WORLD_DB_VERSION = "2016-01-06_04_misc_creature";
 
 class Master : public Singleton<Master>
 {
