3591862237682b095ec2071fe2f300be8e353a04
Zyres <mr@eichbuehlerhof.de>
2016-05-05 19:50:49 +0200

    Codestyle: Prevent null pointer dereference.

--- src/world/Commands/NpcCommands.cpp
+++ src/world/Commands/NpcCommands.cpp
@@ -8,23 +8,29 @@ This file is released under the MIT license. See README-MIT for more information
 // Zyres: not only for npc!
 bool ChatHandler::HandlePossessCommand(const char* args, WorldSession* m_session)
 {
-    Unit* pTarget = GetSelectedUnit(m_session);
-    if (pTarget != nullptr)
+    Unit* unit_target = GetSelectedUnit(m_session);
+    if (unit_target != nullptr)
     {
-        if (pTarget->IsPet() || pTarget->GetCreatedByGUID() != 0)
+        if (unit_target->IsPet() || unit_target->GetCreatedByGUID() != 0)
         {
             RedSystemMessage(m_session, "You can not possess a pet!");
             return false;
         }
-        else if (pTarget->IsPlayer())
+        else if (unit_target->IsPlayer())
         {
-            Player* player = static_cast<Player*>(pTarget);
+            Player* player = static_cast<Player*>(unit_target);
+            if (player == nullptr)
+                return true;
+
             BlueSystemMessage(m_session, "Player %s selected.", player->GetName());
             sGMLog.writefromsession(m_session, "used possess command on PLAYER %s", player->GetName());
         }
-        else if (pTarget->IsCreature())
+        else if (unit_target->IsCreature())
         {
-            Creature* creature = static_cast<Creature*>(pTarget);
+            Creature* creature = static_cast<Creature*>(unit_target);
+            if (creature == nullptr)
+                return true;
+
             BlueSystemMessage(m_session, "Creature %s selected.", creature->GetCreatureInfo()->Name);
             sGMLog.writefromsession(m_session, "used possess command on Creature spawn_id %u", creature->GetCreatureInfo()->Name, creature->GetSQL_id());
         }
@@ -35,7 +41,7 @@ bool ChatHandler::HandlePossessCommand(const char* args, WorldSession* m_session
         return true;
     }
 
-    m_session->GetPlayer()->Possess(pTarget);
+    m_session->GetPlayer()->Possess(unit_target);
 
     return true;
 }
