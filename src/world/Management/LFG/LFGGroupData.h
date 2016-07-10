562732514aadc09ae838ec2d8acc2f712386609f
Zyres <mr@eichbuehlerhof.de>
2016-07-10 17:10:28 +0200

    Forget LFG files

---
+++ src/world/Management/LFG/LFGGroupData.h
@@ -0,0 +1,67 @@
+/*
+ * AscEmu Framework based on ArcEmu MMORPG Server
+ * Copyright (C) 2014-2016 AscEmu Team <http://www.ascemu.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LFGGROUPDATA_H
+#define _LFGGROUPDATA_H
+
+#include "Lfg.h"
+
+enum LfgGroupEnum
+{
+    LFG_GROUP_MAX_KICKS         = 3,
+    LFG_GROUP_KICK_VOTES_NEEDED = 3
+};
+
+/// Stores all lfg data needed about a group.
+class LfgGroupData
+{
+    public:
+
+        LfgGroupData();
+        ~LfgGroupData();
+
+        // General
+        void SetState(LfgState state);
+        void RestoreState();
+        // Dungeon
+        void SetDungeon(uint32 dungeon);
+        // VoteKick
+        void SetVotesNeeded(uint8 votes);
+        void DecreaseKicksLeft();
+
+        // General
+        LfgState GetState() const;
+        // Dungeon
+        uint32 GetDungeon(bool asId = true) const;
+        // VoteKick
+        uint8 GetVotesNeeded() const;
+        uint8 GetKicksLeft() const;
+
+    private:
+
+        // General
+        LfgState m_State;       ///< State if group in LFG
+        LfgState m_OldState;    ///< Old State
+        // Dungeon
+        uint32 m_Dungeon;       ///< Dungeon entry
+        // Vote Kick
+        uint8 m_VotesNeeded;    ///< Votes need to kick success
+        uint8 m_KicksLeft;      ///< Number of kicks left
+};
+
+#endif      // _LFGGROUPDATA_H
