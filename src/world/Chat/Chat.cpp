87dc304528be7932b341e88511bae8b29929ff4d
Zyres <mr@eichbuehlerhof.de>
2016-07-21 18:14:21 +0200

    Moved last two functions in Level1.cpp to Chat.cpp Removed Level1.cpp

--- src/world/Chat/Chat.cpp
+++ src/world/Chat/Chat.cpp
@@ -1871,3 +1871,63 @@ bool ChatHandler::HandleCommandsCommand(const char* args, WorldSession* m_sessio
 
     return true;
 }
+
+uint16 GetItemIDFromLink(const char* itemlink, uint32* itemid)
+{
+    if (itemlink == NULL)
+    {
+        *itemid = 0;
+        return 0;
+    }
+    uint16 slen = (uint16)strlen(itemlink);
+    const char* ptr = strstr(itemlink, "|Hitem:");
+    if (ptr == NULL)
+    {
+        *itemid = 0;
+        return slen;
+    }
+    ptr += 7;                       // item id is just past "|Hitem:" (7 bytes)
+    *itemid = atoi(ptr);
+    ptr = strstr(itemlink, "|r");   // the end of the item link
+    if (ptr == NULL)                // item link was invalid
+    {
+        *itemid = 0;
+        return slen;
+    }
+    ptr += 2;
+    return (ptr - itemlink) & 0x0000ffff;
+}
+
+/// DGM: Get skill level command for getting information about a skill
+bool ChatHandler::HandleGetSkillLevelCommand(const char* args, WorldSession* m_session)
+{
+    uint32 skill = 0;
+    char* pSkill = strtok((char*)args, " ");
+    if (!pSkill)
+        return false;
+    else
+        skill = atol(pSkill);
+    Player* plr = GetSelectedPlayer(m_session, true, true);
+    if (!plr) return false;
+    if (skill > SkillNameManager->maxskill)
+    {
+        BlueSystemMessage(m_session, "Skill: %u does not exists", skill);
+        return false;
+    }
+    char* SkillName = SkillNameManager->SkillNames[skill];
+    if (SkillName == 0)
+    {
+        BlueSystemMessage(m_session, "Skill: %u does not exists", skill);
+        return false;
+    }
+    if (!plr->_HasSkillLine(skill))
+    {
+        BlueSystemMessage(m_session, "Player does not have %s skill.", SkillName);
+        return false;
+    }
+    uint32 nobonus = plr->_GetSkillLineCurrent(skill, false);
+    uint32 bonus = plr->_GetSkillLineCurrent(skill, true) - nobonus;
+    uint32 max = plr->_GetSkillLineMax(skill);
+    BlueSystemMessage(m_session, "Player's %s skill has level: %u maxlevel: %u. (+ %u bonus)", SkillName, nobonus, max, bonus);
+    return true;
+}
