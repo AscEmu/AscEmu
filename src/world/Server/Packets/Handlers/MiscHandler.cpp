7ff5679def8345ae48ca07457d628aafc86f8ffc
DankoDJ <matzesper@aol.com>
2016-09-10 10:10:50 +0200

    Read guid for corpses

--- src/world/Server/Packets/Handlers/MiscHandler.cpp
+++ src/world/Server/Packets/Handlers/MiscHandler.cpp
@@ -1111,8 +1111,28 @@ void WorldSession::HandleCorpseReclaimOpcode(WorldPacket& recv_data)
 
     LOG_DETAIL("WORLD: Received CMSG_RECLAIM_CORPSE");
 
-    uint64 guid;
-    recv_data >> guid;
+    ObjectGuid guid;
+    
+    guid[1] = recv_data.readBit();
+    guid[5] = recv_data.readBit();
+    guid[7] = recv_data.readBit();
+    guid[2] = recv_data.readBit();
+    guid[6] = recv_data.readBit();
+    guid[3] = recv_data.readBit();
+    guid[0] = recv_data.readBit();
+    guid[4] = recv_data.readBit();
+    
+    recv_data.ReadByteSeq(guid[2]);
+    recv_data.ReadByteSeq(guid[5]);
+    recv_data.ReadByteSeq(guid[4]);
+    recv_data.ReadByteSeq(guid[6]);
+    recv_data.ReadByteSeq(guid[1]);
+    recv_data.ReadByteSeq(guid[0]);
+    recv_data.ReadByteSeq(guid[7]);
+    recv_data.ReadByteSeq(guid[3]);
+
+    if (guid == 0)
+        return;
 
     Corpse* pCorpse = objmgr.GetCorpse((uint32)guid);
     if (pCorpse == nullptr)
