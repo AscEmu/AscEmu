3fff36bdb29c554d5541946182532f5106568d39
Zyres Ascemu <mr@eichbuehlerhof.de>
2016-10-15 01:51:30 +0200

    Added new files for further commits

---
+++ src/world/Spell/SpellDefines.hpp
@@ -0,0 +1,9 @@
+/*
+Copyright (c) 2014-2016 AscEmu Team <http://www.ascemu.org/>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#ifndef _SPELL_DEFINES_HPP
+#define _SPELL_DEFINES_HPP
+
+#endif
