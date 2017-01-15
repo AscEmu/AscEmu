538848f6eb47ea431164950551cb9db142e916fc
Zyres <mr@eichbuehlerhof.de>
2017-01-15 18:28:29 +0100

    Resolved error: cannot pass object of non-trivial type

--- src/world/Server/Packets/Handlers/SpellHandler.cpp
+++ src/world/Server/Packets/Handlers/SpellHandler.cpp
@@ -375,7 +375,7 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
         //autoshot 75
         if ((spellInfo->AttributesExB & ATTRIBUTESEXB_ACTIVATE_AUTO_SHOT) /*spellInfo->Attributes == 327698*/)	// auto shot..
         {
-            LogDebugFlag(LF_SPELL, "HandleCastSpellOpcode : Auto Shot-type spell cast (id %u, name %s)" , spellInfo->Id , spellInfo->Name);
+            LogDebugFlag(LF_SPELL, "HandleCastSpellOpcode : Auto Shot-type spell cast (id %u, name %s)" , spellInfo->Id , spellInfo->Name.c_str());
             Item* weapon = GetPlayer()->GetItemInterface()->GetInventoryItem(EQUIPMENT_SLOT_RANGED);
             if (!weapon)
                 return;
