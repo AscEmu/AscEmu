6b9b58bd9388b1399f8538190a407360a9caf0e7
Zyres <mr@eichbuehlerhof.de>
2017-01-28 13:28:37 +0100

    Updated spawns for map 543. Close #330

--- src/world/Server/Master.h
+++ src/world/Server/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2016-03-28_01_gm_survey";
-static const char* REQUIRED_WORLD_DB_VERSION = "2017-01-11_01_ai_agents";
+static const char* REQUIRED_WORLD_DB_VERSION = "2017-01-28_01_creature_spawns";
 
 class Master : public Singleton<Master>
 {
