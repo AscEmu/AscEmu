a8e3e2df1fc1f578f6ceb31b632410da6c4e44ff
Zyres <mr@eichbuehlerhof.de>
2017-01-30 05:05:09 +0100

    Multiversion: map extractor tbc and wotlk

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -19,6 +19,8 @@
 
 #define _CRT_SECURE_NO_DEPRECATE
 
+#include "../../src/world/WorldConf.h"
+
 #include <stdio.h>
 #include <deque>
 #include <set>
@@ -92,7 +94,17 @@ float CONF_flat_height_delta_limit = 0.005f; // If max - min less this value - s
 float CONF_flat_liquid_delta_limit = 0.001f; // If max - min less this value - liquid surface is flat
 
 // List MPQ for extract from
-const char *CONF_mpq_list[]={
+#ifdef AE_TBC
+const char* CONF_mpq_list[] = {
+    "common.MPQ",
+    "expansion.MPQ",
+    "patch.MPQ",
+    "patch-2.MPQ",
+};
+#endif
+
+#ifdef AE_WOTLK
+const char* CONF_mpq_list[] = {
     "common.MPQ",
     "common-2.MPQ",
     "lichking.MPQ",
@@ -100,9 +112,8 @@ const char *CONF_mpq_list[]={
     "patch.MPQ",
     "patch-2.MPQ",
     "patch-3.MPQ",
-    "patch-4.MPQ",
-    "patch-5.MPQ",
 };
+#endif
 
 static const char* const langs[] = {"enGB", "enUS", "deDE", "esES", "frFR", "koKR", "zhCN", "zhTW", "enCN", "enTW", "esMX", "ruRU" };
 #define LANG_COUNT 12
