f928cb3613392ae8ee8b2d69d5a1e7ebbe7f23e2
Zyres <mr@eichbuehlerhof.de>
2017-02-07 18:24:31 +0100

    Removed unused function from class DatabaseCleaner

--- src/world/Storage/DatabaseCleaner.cpp
+++ src/world/Storage/DatabaseCleaner.cpp
@@ -31,19 +31,7 @@ void DatabaseCleaner::Run()
 {
     LogNotice("DatabaseCleaner : Stage 1 of 3: Cleaning characters...");
     CleanCharacters();
-
-    LogNotice("DatabaseCleaner : Stage 2 of 3: Cleaning world...");
-    CleanWorld();
-
-    LogNotice("DatabaseCleaner : Stage 3 of 3: Optimizing databases...");
-    Optimize();
 }
-
-void DatabaseCleaner::CleanWorld()
-{}
-
-void DatabaseCleaner::Optimize()
-{}
 
 void DatabaseCleaner::CleanCharacters()
 {
