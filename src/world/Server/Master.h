228e6f4dba54cef59fe2a16c38518a79f193f780
Zyres <mr@eichbuehlerhof.de>
2017-02-25 13:57:38 +0100

    Update go rotation. Close #418

--- src/world/Server/Master.h
+++ src/world/Server/Master.h
@@ -28,7 +28,7 @@
 #include "../shared/AscemuServerDefines.hpp"
 
 static const char* REQUIRED_CHAR_DB_VERSION = "2017-02-04_01_queue_tables";
-static const char* REQUIRED_WORLD_DB_VERSION = "2017-02-06_01_creature_spawns";
+static const char* REQUIRED_WORLD_DB_VERSION = "2017-02-25_01_gameobject_spawns";
 
 class Master : public Singleton<Master>
 {
