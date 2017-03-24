a7fbe9abbfe4aa73ee91edd511c270f016dcd220
Zyres <mr@eichbuehlerhof.de>
2017-03-24 22:10:36 +0100

    Cata: Kezan spawns/quests

--- src/world/Server/Master.cpp
+++ src/world/Server/Master.cpp
@@ -54,7 +54,7 @@ static const char* REQUIRED_CHAR_DB_VERSION = "2017-03-21_01_characters";
 static const char* REQUIRED_WORLD_DB_VERSION = "2017-02-25_01_gameobject_spawns";
 #else
 static const char* REQUIRED_CHAR_DB_VERSION = "2017-03-21_01_characters";
-static const char* REQUIRED_WORLD_DB_VERSION = "2017-03-24_01_startup_errors";
+static const char* REQUIRED_WORLD_DB_VERSION = "2017-03-24_02_kezan";
 #endif
 
 void Master::_OnSignal(int s)
