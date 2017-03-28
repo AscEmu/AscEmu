0bc26be432452ef1aad8a7beee40a293e46d0bbb
Zyres <mr@eichbuehlerhof.de>
2017-03-28 17:00:53 +0200

    Cata: Update world spawns from branch 4.3.4

--- src/world/Server/Master.cpp
+++ src/world/Server/Master.cpp
@@ -54,7 +54,7 @@ static const char* REQUIRED_CHAR_DB_VERSION = "2017-03-21_01_characters";
 static const char* REQUIRED_WORLD_DB_VERSION = "2017-02-25_01_gameobject_spawns";
 #else
 static const char* REQUIRED_CHAR_DB_VERSION = "2017-03-21_01_characters";
-static const char* REQUIRED_WORLD_DB_VERSION = "2017-03-24_03_spell_defines";
+static const char* REQUIRED_WORLD_DB_VERSION = "2017-03-26_01_spawns";
 #endif
 
 void Master::_OnSignal(int s)
