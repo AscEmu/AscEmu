1c3f11c805e8746411deb9c7789c0befd625d23b
Zyres <mr@eichbuehlerhof.de>
2017-09-25 18:04:28 +0200

    Added project MapScript This project will replace InstanceScripts.

---
+++ src/scripts/MapScripts/Setup.cpp
@@ -0,0 +1,35 @@
+/*
+Copyright (c) 2014-2017 AscEmu Team <http://www.ascemu.org/>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#include "Setup.h"
+#include "Server/Script/ScriptSetup.h"
+
+#define SKIP_ALLOCATOR_SHARING 1
+
+extern "C" {
+    SCRIPT_DECL void _exp_set_serverstate_singleton(ServerState* serverState)
+    {
+        ServerState::instance(serverState);
+    }
+
+    SCRIPT_DECL uint32_t _exp_get_script_type()
+    {
+        return SCRIPT_TYPE_MISC;
+    }
+
+    SCRIPT_DECL void _exp_script_register(ScriptMgr* scriptMgr)
+    {
+        //ExampleScripts(scriptMgr);
+    }
+}
+
+#ifdef WIN32
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    return TRUE;
+}
+
+#endif  //Win32
---
+++ src/scripts/MapScripts/Setup.h
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2014-2017 AscEmu Team <http://www.ascemu.org/>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#pragma once
+
+#include "Units/Creatures/AIInterface.h"
+#include "Management/Item.h"
+#include "Map/MapMgr.h"
+#include "Management/ItemInterface.h"
+#include "Storage/MySQLDataStore.hpp"
+#include <Management/QuestLogEntry.hpp>
+#include "Map/MapScriptInterface.h"
+#include "Spell/Customization/SpellCustomizations.hpp"
+#include "../Common/Instance_Base.h"
+#include "../Common/Base.h"
+
+
+//void ExampleScript(ScriptMgr* scriptMgr);
+
