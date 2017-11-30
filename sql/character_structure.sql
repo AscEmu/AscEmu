2621c25570b28945fe0f880bdb0c26e45fd8c232
Zyres <mr@eichbuehlerhof.de>
2017-11-30 18:42:14 +0100

    CahracterDB: Updated last db version.

--- sql/character_structure.sql
+++ sql/character_structure.sql
@@ -301,7 +301,7 @@ CREATE TABLE IF NOT EXISTS `character_db_version` (
 -- Dumping data for table character_db_version: ~1 rows (approximately)
 /*!40000 ALTER TABLE `character_db_version` DISABLE KEYS */;
 INSERT INTO `character_db_version` (`LastUpdate`) VALUES
-	('2017-06-25_01_clienbtaddons');
+	('2017-09-13_01_account_permissions');
 /*!40000 ALTER TABLE `character_db_version` ENABLE KEYS */;
 
 
