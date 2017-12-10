1aed369c38708514463533cfb46b794331bbeee2
schnek <tolstikov.misha@gmail.com>
2017-12-10 21:52:55 +0300

    Updated last file for world_db

--- sql/world_updates/4_Cata/2017-12-10_01_creature_spawns.sql
+++ sql/world_updates/4_Cata/2017-12-10_01_creature_spawns.sql
@@ -6,4 +6,4 @@ DELETE FROM `creature_spawns` WHERE  `id`=139057;
 DELETE FROM `creature_spawns` WHERE  `id`=139952;
     
 -- Set last world sql update
-UPDATE `world_db_version` SET `LastUpdate` = '2017-11-09_01_npc_script_text' WHERE `LastUpdate` = '2017-12-10_01_creature_spawns';
+UPDATE `world_db_version` SET `LastUpdate` = '2017-12-10_01_creature_spawns' WHERE `LastUpdate` = '2017-11-09_01_npc_script_text';
