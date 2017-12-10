50df08520428a1be8662ad5da854d84ae8d914a4
schnek <tolstikov.misha@gmail.com>
2017-12-10 21:04:44 +0300

    remove spawn event The Battle For The Undercity
      spawn incorrect

---
+++ sql/world_updates/3_WotLK/2017-12-10_01_creature_spawns.sql
@@ -0,0 +1,9 @@
+-- 
+-- Delete creature_spawns Event : The Battle For The Undercity
+--
+
+DELETE FROM `creature_spawns` WHERE  `id`=139057;
+DELETE FROM `creature_spawns` WHERE  `id`=139952;
+    
+-- Set last world sql update
+UPDATE `world_db_version` SET `LastUpdate` = '2017-11-09_01_npc_script_text' WHERE `LastUpdate` = '2017-12-10_01_creature_spawns';
