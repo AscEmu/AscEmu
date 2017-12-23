11216d00db2ac77a0b1ae8002c3fceefeec4fb37
Zyres <mr@eichbuehlerhof.de>
2017-12-23 19:49:05 +0100

    Added missing copyrigth in AEThreading files

--- src/shared/Threading/AEThread.h
+++ src/shared/Threading/AEThread.h
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2014-2017 AscEmu Team <http://www.ascemu.org/>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
 #pragma once
 #include "ThreadState.h"
 
