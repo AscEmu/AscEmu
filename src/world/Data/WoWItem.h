f0a81ebbcde7d8f4ccf476a824a85691376c4bac
Zyres <mr@eichbuehlerhof.de>
2018-02-18 23:12:11 +0100

    Split WoWItem structures and verify struct for 1.12

--- src/world/Data/WoWItem.h
+++ src/world/Data/WoWItem.h
@@ -13,20 +13,13 @@ This file is released under the MIT license. See README-MIT for more information
 
 #pragma once
 #include "WoWObject.h"
-
-#define WOWITEM_SPELL_CHARGES_COUNT 5
+#pragma pack(push, 1)
 
 #if VERSION_STRING == Classic
-#define WOWITEM_ENCHANTMENT_COUNT 21
-#elif VERSION_STRING == TBC
-#define WOWITEM_ENCHANTMENT_COUNT 33
-#elif VERSION_STRING == WotLK
-#define WOWITEM_ENCHANTMENT_COUNT 35
-#elif VERSION_STRING == Cata
-#define WOWITEM_ENCHANTMENT_COUNT 44
-#endif
 
-#pragma pack(push, 1)
+#define WOWITEM_SPELL_CHARGES_COUNT 5
+#define WOWITEM_ENCHANTMENT_COUNT 21
+
 struct WoWItem : WoWObject
 {
     union
@@ -49,13 +42,109 @@ struct WoWItem : WoWObject
     uint32_t enchantment[WOWITEM_ENCHANTMENT_COUNT];
     uint32_t property_seed;
     uint32_t random_properties_id;
-#if VERSION_STRING != Cata
     uint32_t item_text_id;
-#endif
     uint32_t durability;
     uint32_t max_durability;
-#if VERSION_STRING == Cata
-    uint32_t create_played_time;
-#endif
 };
+#endif
+
+#if VERSION_STRING == TBC
+
+#define WOWITEM_SPELL_CHARGES_COUNT 5
+#define WOWITEM_ENCHANTMENT_COUNT 33
+
+struct WoWItem : WoWObject
+{
+    union
+    {
+        struct
+        {
+            uint32_t low;
+            uint32_t high;
+        } owner_guid_parts;
+
+        uint64_t owner_guid;
+    };
+    uint64_t container_guid;
+    uint64_t creator_guid;
+    uint64_t gift_creator_guid;
+    uint32_t stack_count;
+    uint32_t duration;
+    int32_t spell_charges[WOWITEM_SPELL_CHARGES_COUNT];
+    uint32_t flags;
+    uint32_t enchantment[WOWITEM_ENCHANTMENT_COUNT];
+    uint32_t property_seed;
+    uint32_t random_properties_id;
+    uint32_t item_text_id;
+    uint32_t durability;
+    uint32_t max_durability;
+};
+#endif
+
+#if VERSION_STRING == WotLK
+
+#define WOWITEM_SPELL_CHARGES_COUNT 5
+#define WOWITEM_ENCHANTMENT_COUNT 35
+
+struct WoWItem : WoWObject
+{
+    union
+    {
+        struct
+        {
+            uint32_t low;
+            uint32_t high;
+        } owner_guid_parts;
+
+        uint64_t owner_guid;
+    };
+    uint64_t container_guid;
+    uint64_t creator_guid;
+    uint64_t gift_creator_guid;
+    uint32_t stack_count;
+    uint32_t duration;
+    int32_t spell_charges[WOWITEM_SPELL_CHARGES_COUNT];
+    uint32_t flags;
+    uint32_t enchantment[WOWITEM_ENCHANTMENT_COUNT];
+    uint32_t property_seed;
+    uint32_t random_properties_id;
+    uint32_t item_text_id;
+    uint32_t durability;
+    uint32_t max_durability;
+};
+#endif
+
+#if VERSION_STRING == Cata
+
+#define WOWITEM_SPELL_CHARGES_COUNT 5
+#define WOWITEM_ENCHANTMENT_COUNT 44
+
+struct WoWItem : WoWObject
+{
+    union
+    {
+        struct
+        {
+            uint32_t low;
+            uint32_t high;
+        } owner_guid_parts;
+
+        uint64_t owner_guid;
+    };
+    uint64_t container_guid;
+    uint64_t creator_guid;
+    uint64_t gift_creator_guid;
+    uint32_t stack_count;
+    uint32_t duration;
+    int32_t spell_charges[WOWITEM_SPELL_CHARGES_COUNT];
+    uint32_t flags;
+    uint32_t enchantment[WOWITEM_ENCHANTMENT_COUNT];
+    uint32_t property_seed;
+    uint32_t random_properties_id;
+    uint32_t durability;
+    uint32_t max_durability;
+    uint32_t create_played_time;
+};
+#endif
+
 #pragma pack(pop)
