c521b2e62beb202c6c0cbba7fad93679bfaa8c65
Zyres <mr@eichbuehlerhof.de>
2018-04-08 00:57:20 +0200

    Removed Cata world_updates

--- sql/world_updates/4_Cata/2017-05-05_02_areatriggers.sql
+++
@@ -1,7 +0,0 @@
---
--- Update Dark Portal target position
---
-UPDATE `areatriggers` SET `position_x` = -248.113, `position_y` = 922.9, `position_z` = 84.3497, `orientation` = 1.5708 WHERE `entry` = 4354;
-
-
-UPDATE `world_db_version` SET `LastUpdate` = '2017-05-05_02_areatriggers' WHERE `LastUpdate` = '2017-05-05_01_playercreateinfo';
