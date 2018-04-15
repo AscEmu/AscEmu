AscEmu/AscEmu

commit a516affd20494bf550bc87de4893c09271d3cfd0

Added Version specific base mana regen values.

src/world/Units/Players/PlayerDefines.hpp
@@ -882,104 +882,208 @@ inline bool isClassRaceCombinationPossible(uint8_t _class, uint8_t _race)
 }
 
 // table from http://www.wowwiki.com/Mana_regeneration
-const static float BaseManaRegen[DBC_PLAYER_LEVEL_CAP] =
+static float BaseManaRegenByAEVersion[165][3] =
 {
-    0.034965f,
-    0.034191f,
-    0.033465f,
-    0.032526f,
-    0.031661f,
-    0.031076f,
-    0.030523f,
-    0.029994f,
-    0.029307f,
-    0.028661f,
-    0.027584f,
-    0.026215f,
-    0.025381f,
-    0.024300f,
-    0.023345f,
-    0.022748f,
-    0.021958f,
-    0.021386f,
-    0.020790f,
-    0.020121f,
-    0.019733f,
-    0.019155f,
-    0.018819f,
-    0.018316f,
-    0.017936f,
-    0.017576f,
-    0.017201f,
-    0.016919f,
-    0.016581f,
-    0.016233f,
-    0.015994f,
-    0.015707f,
-    0.015464f,
-    0.015204f,
-    0.014956f,
-    0.014744f,
-    0.014495f,
-    0.014302f,
-    0.014094f,
-    0.013895f,
-    0.013724f,
-    0.013522f,
-    0.013363f,
-    0.013175f,
-    0.012996f,
-    0.012853f,
-    0.012687f,
-    0.012539f,
-    0.012384f,
-    0.012233f,
-    0.012113f,
-    0.011973f,
-    0.011859f,
-    0.011714f,
-    0.011575f,
-    0.011473f,
-    0.011342f,
-    0.011245f,
-    0.011110f,
-    0.010999f,
-#if VERSION_STRING > Classic
-    0.010700f,
-    0.010522f,
-    0.010290f,
-    0.010119f,
-    0.009968f,
-    0.009808f,
-    0.009651f,
-    0.009553f,
-    0.009445f,
-    0.009327f,
-#endif
-#if VERSION_STRING > TBC
-    0.008859f,
-    0.008415f,
-    0.007993f,
-    0.007592f,
-    0.007211f,
-    0.006849f,
-    0.006506f,
-    0.006179f,
-    0.005869f,
-    0.005575f,
-#endif
-#if VERSION_STRING > WotLK
-    0.003345f,
-    0.003345f,
-    0.003345f,
-    0.003345f,
-    0.003345f,
-#endif
+    //\ brief wrong for classic. Mana regen was calculated based on spirit etc.
+    //       redo this for classic someday.
+
+    // Build: 8089
+    { 1, 0.034965, 5875 },
+    { 2, 0.034191, 5875 },
+    { 3, 0.033465, 5875 },
+    { 4, 0.032526, 5875 },
+    { 5, 0.031661, 5875 },
+    { 6, 0.031076, 5875 },
+    { 7, 0.030523, 5875 },
+    { 8, 0.029994, 5875 },
+    { 9, 0.029307, 5875 },
+    { 10, 0.028661, 5875 },
+    { 11, 0.027584, 5875 },
+    { 12, 0.026215, 5875 },
+    { 13, 0.025381, 5875 },
+    { 14, 0.024300, 5875 },
+    { 15, 0.023345, 5875 },
+    { 16, 0.022748, 5875 },
+    { 17, 0.021958, 5875 },
+    { 18, 0.021386, 5875 },
+    { 19, 0.020790, 5875 },
+    { 20, 0.020121, 5875 },
+    { 21, 0.019733, 5875 },
+    { 22, 0.019155, 5875 },
+    { 23, 0.018819, 5875 },
+    { 24, 0.018316, 5875 },
+    { 25, 0.017936, 5875 },
+    { 26, 0.017576, 5875 },
+    { 27, 0.017201, 5875 },
+    { 28, 0.016919, 5875 },
+    { 29, 0.016581, 5875 },
+    { 30, 0.016233, 5875 },
+    { 31, 0.015994, 5875 },
+    { 32, 0.015707, 5875 },
+    { 33, 0.015464, 5875 },
+    { 34, 0.015204, 5875 },
+    { 35, 0.014956, 5875 },
+    { 36, 0.014744, 5875 },
+    { 37, 0.014495, 5875 },
+    { 38, 0.014302, 5875 },
+    { 39, 0.014094, 5875 },
+    { 40, 0.013895, 5875 },
+    { 41, 0.013724, 5875 },
+    { 42, 0.013522, 5875 },
+    { 43, 0.013363, 5875 },
+    { 44, 0.013175, 5875 },
+    { 45, 0.012996, 5875 },
+    { 46, 0.012853, 5875 },
+    { 47, 0.012687, 5875 },
+    { 48, 0.012539, 5875 },
+    { 49, 0.012384, 5875 },
+    { 50, 0.012233, 5875 },
+    { 51, 0.012113, 5875 },
+    { 52, 0.011973, 5875 },
+    { 53, 0.011859, 5875 },
+    { 54, 0.011714, 5875 },
+    { 55, 0.011575, 5875 },
+    { 56, 0.011473, 5875 },
+    { 57, 0.011342, 5875 },
+    { 58, 0.011245, 5875 },
+    { 59, 0.011110, 5875 },
+    { 60, 0.010999, 5875 },
+
+    // Build: 8089
+    { 61, 0.010700, 8478 },
+    { 62, 0.010522, 8478 },
+    { 63, 0.010290, 8478 },
+    { 64, 0.010119, 8478 },
+    { 65, 0.009968, 8478 },
+    { 66, 0.009808, 8478 },
+    { 67, 0.009651, 8478 },
+    { 68, 0.009553, 8478 },
+    { 69, 0.009445, 8478 },
+    { 70, 0.009327, 8478 },
+
+    //Build: 9056
+    { 71, 0.008859, 12340 },
+    { 72, 0.008415, 12340 },
+    { 73, 0.007993, 12340 },
+    { 74, 0.007592, 12340 },
+    { 75, 0.007211, 12340 },
+    { 76, 0.006849, 12340 },
+    { 77, 0.006506, 12340 },
+    { 78, 0.006179, 12340 },
+    { 79, 0.005869, 12340 },
+    { 80, 0.005575, 12340 },
+
+    // Build: 13164
+    { 1, 0.020979, 15595 },
+    { 2, 0.020515, 15595 },
+    { 3, 0.020079, 15595 },
+    { 4, 0.019516, 15595 },
+    { 5, 0.018997, 15595 },
+    { 6, 0.018646, 15595 },
+    { 7, 0.018314, 15595 },
+    { 8, 0.017997, 15595 },
+    { 9, 0.017584, 15595 },
+    { 10, 0.017197, 15595 },
+    { 11, 0.016551, 15595 },
+    { 12, 0.015729, 15595 },
+    { 13, 0.015229, 15595 },
+    { 14, 0.014580, 15595 },
+    { 15, 0.014008, 15595 },
+    { 16, 0.013650, 15595 },
+    { 17, 0.013175, 15595 },
+    { 18, 0.012832, 15595 },
+    { 19, 0.012475, 15595 },
+    { 20, 0.012073, 15595 },
+    { 21, 0.011840, 15595 },
+    { 22, 0.011494, 15595 },
+    { 23, 0.011292, 15595 },
+    { 24, 0.010990, 15595 },
+    { 25, 0.010761, 15595 },
+    { 26, 0.010546, 15595 },
+    { 27, 0.010321, 15595 },
+    { 28, 0.010151, 15595 },
+    { 29, 0.009949, 15595 },
+    { 30, 0.009740, 15595 },
+    { 31, 0.009597, 15595 },
+    { 32, 0.009425, 15595 },
+    { 33, 0.009278, 15595 },
+    { 34, 0.009123, 15595 },
+    { 35, 0.008974, 15595 },
+    { 36, 0.008847, 15595 },
+    { 37, 0.008698, 15595 },
+    { 38, 0.008581, 15595 },
+    { 39, 0.008457, 15595 },
+    { 40, 0.008338, 15595 },
+    { 41, 0.008235, 15595 },
+    { 42, 0.008113, 15595 },
+    { 43, 0.008018, 15595 },
+    { 44, 0.007906, 15595 },
+    { 45, 0.007798, 15595 },
+    { 46, 0.007713, 15595 },
+    { 47, 0.007612, 15595 },
+    { 48, 0.007524, 15595 },
+    { 49, 0.007430, 15595 },
+    { 50, 0.007340, 15595 },
+    { 51, 0.007268, 15595 },
+    { 52, 0.007184, 15595 },
+    { 53, 0.007116, 15595 },
+    { 54, 0.007029, 15595 },
+    { 55, 0.006945, 15595 },
+    { 56, 0.006884, 15595 },
+    { 57, 0.006805, 15595 },
+    { 58, 0.006747, 15595 },
+    { 59, 0.006667, 15595 },
+    { 60, 0.006600, 15595 },
+    { 61, 0.006421, 15595 },
+    { 62, 0.006314, 15595 },
+    { 63, 0.006175, 15595 },
+    { 64, 0.006072, 15595 },
+    { 65, 0.005981, 15595 },
+    { 66, 0.005885, 15595 },
+    { 67, 0.005791, 15595 },
+    { 68, 0.005732, 15595 },
+    { 69, 0.005668, 15595 },
+    { 70, 0.005596, 15595 },
+    { 71, 0.005316, 15595 },
+    { 72, 0.005049, 15595 },
+    { 73, 0.004796, 15595 },
+    { 74, 0.004555, 15595 },
+    { 75, 0.004327, 15595 },
+    { 76, 0.004110, 15595 },
+    { 77, 0.003903, 15595 },
+    { 78, 0.003708, 15595 },
+    { 79, 0.003522, 15595 },
+    { 80, 0.003345, 15595 },
+    { 81, 0.003345, 15595 },
+    { 82, 0.003345, 15595 },
+    { 83, 0.003345, 15595 },
+    { 84, 0.003345, 15595 },
+    { 85, 0.003345, 15595 },
 };
 
-static uint8_t getSideByRace(uint8_t side)
+inline float getBaseManaRegen(uint32_t level)
 {
-    switch (side)
+    // classic   = 60
+    // tbc      += 10
+    // wotlk    += 10
+    // cata     += 85
+
+    for (uint8_t i = 0; i < 165; ++i)
+    {
+        if (BaseManaRegenByAEVersion[i][0] == level)
+        {
+            if (BaseManaRegenByAEVersion[i][2] == getAEVersion())
+                return BaseManaRegenByAEVersion[i][1];
+        }
+    }
+
+    return 0.f;
+}
+
+
+static uint8_t getSideByRace(uint8_t race)
+{
+    switch (race)
     {
         case RACE_HUMAN:
         case RACE_DWARF:

src/world/Units/Players/Player.Legacy.cpp
@@ -6653,12 +6653,12 @@ void Player::UpdateStats()
 
         if (level > DBC_PLAYER_LEVEL_CAP)
             level = DBC_PLAYER_LEVEL_CAP;
-        //float amt = (0.001f + sqrt((float)Intellect) * Spirit * BaseManaRegen[level-1])*PctPowerRegenModifier[POWER_TYPE_MANA];
+        //float amt = (0.001f + sqrt((float)Intellect) * Spirit * getBaseManaRegen(level))*PctPowerRegenModifier[POWER_TYPE_MANA];
 
         // Mesmer: new Manaregen formula.
         uint32 Spirit = getStat(STAT_SPIRIT);
         uint32 Intellect = getStat(STAT_INTELLECT);
-        float amt = (0.001f + sqrt((float)Intellect) * Spirit * BaseManaRegen[level - 1]) * PctPowerRegenModifier[POWER_TYPE_MANA];
+        float amt = (0.001f + sqrt((float)Intellect) * Spirit * getBaseManaRegen(level)) * PctPowerRegenModifier[POWER_TYPE_MANA];
 #if VERSION_STRING > TBC
         setFloatValue(UNIT_FIELD_POWER_REGEN_FLAT_MODIFIER, amt + m_ModInterrMRegen * 0.2f);
         setFloatValue(UNIT_FIELD_POWER_REGEN_INTERRUPTED_FLAT_MODIFIER, amt * m_ModInterrMRegenPCT / 100.0f + m_ModInterrMRegen * 0.2f);

commit aa670ed8624cff98dbc8b81a045278276cda6d91

Corrected wrong logic introduced in a516affd20494bf550bc87de4893c09271d3cfd0 * resolved warning (double to float)

src/world/Units/Players/PlayerDefines.hpp
@@ -888,177 +888,177 @@ static float BaseManaRegenByAEVersion[165][3] =
     //       redo this for classic someday.
 
     // Build: 8089
-    { 1, 0.034965, 5875 },
-    { 2, 0.034191, 5875 },
-    { 3, 0.033465, 5875 },
-    { 4, 0.032526, 5875 },
-    { 5, 0.031661, 5875 },
-    { 6, 0.031076, 5875 },
-    { 7, 0.030523, 5875 },
-    { 8, 0.029994, 5875 },
-    { 9, 0.029307, 5875 },
-    { 10, 0.028661, 5875 },
-    { 11, 0.027584, 5875 },
-    { 12, 0.026215, 5875 },
-    { 13, 0.025381, 5875 },
-    { 14, 0.024300, 5875 },
-    { 15, 0.023345, 5875 },
-    { 16, 0.022748, 5875 },
-    { 17, 0.021958, 5875 },
-    { 18, 0.021386, 5875 },
-    { 19, 0.020790, 5875 },
-    { 20, 0.020121, 5875 },
-    { 21, 0.019733, 5875 },
-    { 22, 0.019155, 5875 },
-    { 23, 0.018819, 5875 },
-    { 24, 0.018316, 5875 },
-    { 25, 0.017936, 5875 },
-    { 26, 0.017576, 5875 },
-    { 27, 0.017201, 5875 },
-    { 28, 0.016919, 5875 },
-    { 29, 0.016581, 5875 },
-    { 30, 0.016233, 5875 },
-    { 31, 0.015994, 5875 },
-    { 32, 0.015707, 5875 },
-    { 33, 0.015464, 5875 },
-    { 34, 0.015204, 5875 },
-    { 35, 0.014956, 5875 },
-    { 36, 0.014744, 5875 },
-    { 37, 0.014495, 5875 },
-    { 38, 0.014302, 5875 },
-    { 39, 0.014094, 5875 },
-    { 40, 0.013895, 5875 },
-    { 41, 0.013724, 5875 },
-    { 42, 0.013522, 5875 },
-    { 43, 0.013363, 5875 },
-    { 44, 0.013175, 5875 },
-    { 45, 0.012996, 5875 },
-    { 46, 0.012853, 5875 },
-    { 47, 0.012687, 5875 },
-    { 48, 0.012539, 5875 },
-    { 49, 0.012384, 5875 },
-    { 50, 0.012233, 5875 },
-    { 51, 0.012113, 5875 },
-    { 52, 0.011973, 5875 },
-    { 53, 0.011859, 5875 },
-    { 54, 0.011714, 5875 },
-    { 55, 0.011575, 5875 },
-    { 56, 0.011473, 5875 },
-    { 57, 0.011342, 5875 },
-    { 58, 0.011245, 5875 },
-    { 59, 0.011110, 5875 },
-    { 60, 0.010999, 5875 },
+    { 1, 0.034965f, 5875 },
+    { 2, 0.034191f, 5875 },
+    { 3, 0.033465f, 5875 },
+    { 4, 0.032526f, 5875 },
+    { 5, 0.031661f, 5875 },
+    { 6, 0.031076f, 5875 },
+    { 7, 0.030523f, 5875 },
+    { 8, 0.029994f, 5875 },
+    { 9, 0.029307f, 5875 },
+    { 10, 0.028661f, 5875 },
+    { 11, 0.027584f, 5875 },
+    { 12, 0.026215f, 5875 },
+    { 13, 0.025381f, 5875 },
+    { 14, 0.024300f, 5875 },
+    { 15, 0.023345f, 5875 },
+    { 16, 0.022748f, 5875 },
+    { 17, 0.021958f, 5875 },
+    { 18, 0.021386f, 5875 },
+    { 19, 0.020790f, 5875 },
+    { 20, 0.020121f, 5875 },
+    { 21, 0.019733f, 5875 },
+    { 22, 0.019155f, 5875 },
+    { 23, 0.018819f, 5875 },
+    { 24, 0.018316f, 5875 },
+    { 25, 0.017936f, 5875 },
+    { 26, 0.017576f, 5875 },
+    { 27, 0.017201f, 5875 },
+    { 28, 0.016919f, 5875 },
+    { 29, 0.016581f, 5875 },
+    { 30, 0.016233f, 5875 },
+    { 31, 0.015994f, 5875 },
+    { 32, 0.015707f, 5875 },
+    { 33, 0.015464f, 5875 },
+    { 34, 0.015204f, 5875 },
+    { 35, 0.014956f, 5875 },
+    { 36, 0.014744f, 5875 },
+    { 37, 0.014495f, 5875 },
+    { 38, 0.014302f, 5875 },
+    { 39, 0.014094f, 5875 },
+    { 40, 0.013895f, 5875 },
+    { 41, 0.013724f, 5875 },
+    { 42, 0.013522f, 5875 },
+    { 43, 0.013363f, 5875 },
+    { 44, 0.013175f, 5875 },
+    { 45, 0.012996f, 5875 },
+    { 46, 0.012853f, 5875 },
+    { 47, 0.012687f, 5875 },
+    { 48, 0.012539f, 5875 },
+    { 49, 0.012384f, 5875 },
+    { 50, 0.012233f, 5875 },
+    { 51, 0.012113f, 5875 },
+    { 52, 0.011973f, 5875 },
+    { 53, 0.011859f, 5875 },
+    { 54, 0.011714f, 5875 },
+    { 55, 0.011575f, 5875 },
+    { 56, 0.011473f, 5875 },
+    { 57, 0.011342f, 5875 },
+    { 58, 0.011245f, 5875 },
+    { 59, 0.011110f, 5875 },
+    { 60, 0.010999f, 5875 },
 
     // Build: 8089
-    { 61, 0.010700, 8478 },
-    { 62, 0.010522, 8478 },
-    { 63, 0.010290, 8478 },
-    { 64, 0.010119, 8478 },
-    { 65, 0.009968, 8478 },
-    { 66, 0.009808, 8478 },
-    { 67, 0.009651, 8478 },
-    { 68, 0.009553, 8478 },
-    { 69, 0.009445, 8478 },
-    { 70, 0.009327, 8478 },
+    { 61, 0.010700f, 8478 },
+    { 62, 0.010522f, 8478 },
+    { 63, 0.010290f, 8478 },
+    { 64, 0.010119f, 8478 },
+    { 65, 0.009968f, 8478 },
+    { 66, 0.009808f, 8478 },
+    { 67, 0.009651f, 8478 },
+    { 68, 0.009553f, 8478 },
+    { 69, 0.009445f, 8478 },
+    { 70, 0.009327f, 8478 },
 
     //Build: 9056
-    { 71, 0.008859, 12340 },
-    { 72, 0.008415, 12340 },
-    { 73, 0.007993, 12340 },
-    { 74, 0.007592, 12340 },
-    { 75, 0.007211, 12340 },
-    { 76, 0.006849, 12340 },
-    { 77, 0.006506, 12340 },
-    { 78, 0.006179, 12340 },
-    { 79, 0.005869, 12340 },
-    { 80, 0.005575, 12340 },
+    { 71, 0.008859f, 12340 },
+    { 72, 0.008415f, 12340 },
+    { 73, 0.007993f, 12340 },
+    { 74, 0.007592f, 12340 },
+    { 75, 0.007211f, 12340 },
+    { 76, 0.006849f, 12340 },
+    { 77, 0.006506f, 12340 },
+    { 78, 0.006179f, 12340 },
+    { 79, 0.005869f, 12340 },
+    { 80, 0.005575f, 12340 },
 
     // Build: 13164
-    { 1, 0.020979, 15595 },
-    { 2, 0.020515, 15595 },
-    { 3, 0.020079, 15595 },
-    { 4, 0.019516, 15595 },
-    { 5, 0.018997, 15595 },
-    { 6, 0.018646, 15595 },
-    { 7, 0.018314, 15595 },
-    { 8, 0.017997, 15595 },
-    { 9, 0.017584, 15595 },
-    { 10, 0.017197, 15595 },
-    { 11, 0.016551, 15595 },
-    { 12, 0.015729, 15595 },
-    { 13, 0.015229, 15595 },
-    { 14, 0.014580, 15595 },
-    { 15, 0.014008, 15595 },
-    { 16, 0.013650, 15595 },
-    { 17, 0.013175, 15595 },
-    { 18, 0.012832, 15595 },
-    { 19, 0.012475, 15595 },
-    { 20, 0.012073, 15595 },
-    { 21, 0.011840, 15595 },
-    { 22, 0.011494, 15595 },
-    { 23, 0.011292, 15595 },
-    { 24, 0.010990, 15595 },
-    { 25, 0.010761, 15595 },
-    { 26, 0.010546, 15595 },
-    { 27, 0.010321, 15595 },
-    { 28, 0.010151, 15595 },
-    { 29, 0.009949, 15595 },
-    { 30, 0.009740, 15595 },
-    { 31, 0.009597, 15595 },
-    { 32, 0.009425, 15595 },
-    { 33, 0.009278, 15595 },
-    { 34, 0.009123, 15595 },
-    { 35, 0.008974, 15595 },
-    { 36, 0.008847, 15595 },
-    { 37, 0.008698, 15595 },
-    { 38, 0.008581, 15595 },
-    { 39, 0.008457, 15595 },
-    { 40, 0.008338, 15595 },
-    { 41, 0.008235, 15595 },
-    { 42, 0.008113, 15595 },
-    { 43, 0.008018, 15595 },
-    { 44, 0.007906, 15595 },
-    { 45, 0.007798, 15595 },
-    { 46, 0.007713, 15595 },
-    { 47, 0.007612, 15595 },
-    { 48, 0.007524, 15595 },
-    { 49, 0.007430, 15595 },
-    { 50, 0.007340, 15595 },
-    { 51, 0.007268, 15595 },
-    { 52, 0.007184, 15595 },
-    { 53, 0.007116, 15595 },
-    { 54, 0.007029, 15595 },
-    { 55, 0.006945, 15595 },
-    { 56, 0.006884, 15595 },
-    { 57, 0.006805, 15595 },
-    { 58, 0.006747, 15595 },
-    { 59, 0.006667, 15595 },
-    { 60, 0.006600, 15595 },
-    { 61, 0.006421, 15595 },
-    { 62, 0.006314, 15595 },
-    { 63, 0.006175, 15595 },
-    { 64, 0.006072, 15595 },
-    { 65, 0.005981, 15595 },
-    { 66, 0.005885, 15595 },
-    { 67, 0.005791, 15595 },
-    { 68, 0.005732, 15595 },
-    { 69, 0.005668, 15595 },
-    { 70, 0.005596, 15595 },
-    { 71, 0.005316, 15595 },
-    { 72, 0.005049, 15595 },
-    { 73, 0.004796, 15595 },
-    { 74, 0.004555, 15595 },
-    { 75, 0.004327, 15595 },
-    { 76, 0.004110, 15595 },
-    { 77, 0.003903, 15595 },
-    { 78, 0.003708, 15595 },
-    { 79, 0.003522, 15595 },
-    { 80, 0.003345, 15595 },
-    { 81, 0.003345, 15595 },
-    { 82, 0.003345, 15595 },
-    { 83, 0.003345, 15595 },
-    { 84, 0.003345, 15595 },
-    { 85, 0.003345, 15595 },
+    { 1, 0.020979f, 15595 },
+    { 2, 0.020515f, 15595 },
+    { 3, 0.020079f, 15595 },
+    { 4, 0.019516f, 15595 },
+    { 5, 0.018997f, 15595 },
+    { 6, 0.018646f, 15595 },
+    { 7, 0.018314f, 15595 },
+    { 8, 0.017997f, 15595 },
+    { 9, 0.017584f, 15595 },
+    { 10, 0.017197f, 15595 },
+    { 11, 0.016551f, 15595 },
+    { 12, 0.015729f, 15595 },
+    { 13, 0.015229f, 15595 },
+    { 14, 0.014580f, 15595 },
+    { 15, 0.014008f, 15595 },
+    { 16, 0.013650f, 15595 },
+    { 17, 0.013175f, 15595 },
+    { 18, 0.012832f, 15595 },
+    { 19, 0.012475f, 15595 },
+    { 20, 0.012073f, 15595 },
+    { 21, 0.011840f, 15595 },
+    { 22, 0.011494f, 15595 },
+    { 23, 0.011292f, 15595 },
+    { 24, 0.010990f, 15595 },
+    { 25, 0.010761f, 15595 },
+    { 26, 0.010546f, 15595 },
+    { 27, 0.010321f, 15595 },
+    { 28, 0.010151f, 15595 },
+    { 29, 0.009949f, 15595 },
+    { 30, 0.009740f, 15595 },
+    { 31, 0.009597f, 15595 },
+    { 32, 0.009425f, 15595 },
+    { 33, 0.009278f, 15595 },
+    { 34, 0.009123f, 15595 },
+    { 35, 0.008974f, 15595 },
+    { 36, 0.008847f, 15595 },
+    { 37, 0.008698f, 15595 },
+    { 38, 0.008581f, 15595 },
+    { 39, 0.008457f, 15595 },
+    { 40, 0.008338f, 15595 },
+    { 41, 0.008235f, 15595 },
+    { 42, 0.008113f, 15595 },
+    { 43, 0.008018f, 15595 },
+    { 44, 0.007906f, 15595 },
+    { 45, 0.007798f, 15595 },
+    { 46, 0.007713f, 15595 },
+    { 47, 0.007612f, 15595 },
+    { 48, 0.007524f, 15595 },
+    { 49, 0.007430f, 15595 },
+    { 50, 0.007340f, 15595 },
+    { 51, 0.007268f, 15595 },
+    { 52, 0.007184f, 15595 },
+    { 53, 0.007116f, 15595 },
+    { 54, 0.007029f, 15595 },
+    { 55, 0.006945f, 15595 },
+    { 56, 0.006884f, 15595 },
+    { 57, 0.006805f, 15595 },
+    { 58, 0.006747f, 15595 },
+    { 59, 0.006667f, 15595 },
+    { 60, 0.006600f, 15595 },
+    { 61, 0.006421f, 15595 },
+    { 62, 0.006314f, 15595 },
+    { 63, 0.006175f, 15595 },
+    { 64, 0.006072f, 15595 },
+    { 65, 0.005981f, 15595 },
+    { 66, 0.005885f, 15595 },
+    { 67, 0.005791f, 15595 },
+    { 68, 0.005732f, 15595 },
+    { 69, 0.005668f, 15595 },
+    { 70, 0.005596f, 15595 },
+    { 71, 0.005316f, 15595 },
+    { 72, 0.005049f, 15595 },
+    { 73, 0.004796f, 15595 },
+    { 74, 0.004555f, 15595 },
+    { 75, 0.004327f, 15595 },
+    { 76, 0.004110f, 15595 },
+    { 77, 0.003903f, 15595 },
+    { 78, 0.003708f, 15595 },
+    { 79, 0.003522f, 15595 },
+    { 80, 0.003345f, 15595 },
+    { 81, 0.003345f, 15595 },
+    { 82, 0.003345f, 15595 },
+    { 83, 0.003345f, 15595 },
+    { 84, 0.003345f, 15595 },
+    { 85, 0.003345f, 15595 },
 };
 
 inline float getBaseManaRegen(uint32_t level)
@@ -1072,8 +1072,13 @@ inline float getBaseManaRegen(uint32_t level)
     {
         if (BaseManaRegenByAEVersion[i][0] == level)
         {
+#if VERSION_STRING <= WotLK
+            if (BaseManaRegenByAEVersion[i][2] <= getAEVersion())
+                return BaseManaRegenByAEVersion[i][1];
+#else
             if (BaseManaRegenByAEVersion[i][2] == getAEVersion())
                 return BaseManaRegenByAEVersion[i][1];
+#endif
         }
     }
 