67006ada7411531eff95be874a4105867d117e95
Zyres <mr@eichbuehlerhof.de>
2018-04-16 16:23:34 +0200

    Updated to AscEmu/OneDB@eedf67023a0310a840b3fad0d278f10899b5abc4 Close #582

--- src/world/Server/Master.cpp
+++ src/world/Server/Master.cpp
@@ -53,7 +53,7 @@ ConfigMgr Config;
 
 // DB version
 static const char* REQUIRED_CHAR_DB_VERSION = "2017-09-13_01_account_permissions";
-static const char* REQUIRED_WORLD_DB_VERSION = "1.3.31_build_gameobject_spawns";
+static const char* REQUIRED_WORLD_DB_VERSION = "1.3.32_playercreateinfo";
 
 void Master::_OnSignal(int s)
 {
