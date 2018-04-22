c058f7224e85eacf3568113c893d1a3b286b0148
Zyres <mr@eichbuehlerhof.de>
2018-04-22 20:32:06 +0200

    Updated to AscEmu/OneDB@8bace113365441575a8d65ca62b077bc7df1684b

--- src/world/Server/Master.cpp
+++ src/world/Server/Master.cpp
@@ -53,7 +53,7 @@ ConfigMgr Config;
 
 // DB version
 static const char* REQUIRED_CHAR_DB_VERSION = "2017-09-13_01_account_permissions";
-static const char* REQUIRED_WORLD_DB_VERSION = "1.3.39_event_properties";
+static const char* REQUIRED_WORLD_DB_VERSION = "1.3.40_quest_properties";
 
 void Master::_OnSignal(int s)
 {
