5282f68c36c6bbb6bf27abf52286e42c54de4de7
Zyres <mr@eichbuehlerhof.de>
2018-07-27 21:57:12 +0200

    Just messing around with travis

--- cmake/Compilers/gcc.cmake
+++ cmake/Compilers/gcc.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
 
 include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
+CHECK_CXX_COMPILER_FLAG("-std=c++17" COMPILER_SUPPORTS_CXX14)
 
 if(NOT COMPILER_SUPPORTS_CXX14)
     message(FATAL_ERROR "AscEmu requires at least GCC 6.2! Current version ${CMAKE_CXX_COMPILER} does not support c++14 feature")
@@ -12,7 +12,7 @@ add_definitions(-DHAS_CXX0X)
 
 # apply base flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 -O2")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -O2")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -O2")
 
 if (IS_64BIT)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
