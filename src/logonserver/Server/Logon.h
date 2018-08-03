547a4f9d70fa6739bd3a360bed9ea6e072c96a6a
Zyres <mr@eichbuehlerhof.de>
2018-08-03 13:39:13 +0200

    Fix non pch build

--- src/logonserver/Server/Logon.h
+++ src/logonserver/Server/Logon.h
@@ -7,6 +7,7 @@ This file is released under the MIT license. See README-MIT for more information
 
 #include "LogonConfig.h"
 #include "Singleton.h"
+#include "PerformanceCounter.hpp"
 
 class SERVER_DECL Logon : public Singleton<Logon>
 {
