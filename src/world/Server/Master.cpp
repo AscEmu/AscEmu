61c298daed05038ebd74939d5c9a953ffff5b5cf
Zyres <mr@eichbuehlerhof.de>
2018-08-17 23:54:20 +0200

    Added missing cataclysm data to world db

--- src/world/Server/Master.cpp
+++ src/world/Server/Master.cpp
@@ -55,7 +55,7 @@ ConfigMgr Config;
 
 // DB version
 static const char* REQUIRED_CHAR_DB_VERSION = "20180714-00_guild_tables";
-static const char* REQUIRED_WORLD_DB_VERSION = "20180803-02_playercreateinfo_bars";
+static const char* REQUIRED_WORLD_DB_VERSION = "20180817-00_guild_data";
 
 void Master::_OnSignal(int s)
 {
