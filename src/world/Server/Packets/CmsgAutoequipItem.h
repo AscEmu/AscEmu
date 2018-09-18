99cb378a2fad7c3db95c4a450aa134f2573b023f
Zyres <mr@eichbuehlerhof.de>
2018-09-18 20:48:27 +0200

    Serialise CMSG_AUTOEQUIP_ITEM

---
+++ src/world/Server/Packets/CmsgAutoequipItem.h
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#pragma once
+
+#include <cstdint>
+
+#include "ManagedPacket.h"
+#include "WorldPacket.h"
+
+namespace AscEmu { namespace Packets
+{
+    class CmsgAutoequipItem : public ManagedPacket
+    {
+    public:
+        int8_t srcInventorySlot;
+        int8_t srcSlot;
+
+        CmsgAutoequipItem() : CmsgAutoequipItem(0, 0)
+        {
+        }
+
+        CmsgAutoequipItem(int8_t srcInventorySlot, int8_t srcSlot) :
+            ManagedPacket(CMSG_AUTOEQUIP_ITEM, 2),
+            srcInventorySlot(srcInventorySlot),
+            srcSlot(srcSlot)
+        {
+        }
+
+    protected:
+        bool internalSerialise(WorldPacket& packet) override
+        {
+            return false;
+        }
+
+        bool internalDeserialise(WorldPacket& packet) override
+        {
+            packet >> srcInventorySlot >> srcSlot;
+            return true;
+        }
+    };
+}}
