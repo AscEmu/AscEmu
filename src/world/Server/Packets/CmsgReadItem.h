02d2eef9dd1b26f5d16f8cd3be16fd73f1da1d54
Zyres <mr@eichbuehlerhof.de>
2018-09-19 17:03:06 +0200

    Serialise CMSG_READ_ITEM

---
+++ src/world/Server/Packets/CmsgReadItem.h
@@ -0,0 +1,43 @@
+/*
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#pragma once
+#include <cstdint>
+
+#include "ManagedPacket.h"
+
+namespace AscEmu { namespace Packets
+{
+    class CmsgReadItem : public ManagedPacket
+    {
+    public:
+        int8_t srcContainerSlot;
+        int8_t srcSlot;
+
+        CmsgReadItem() : CmsgReadItem(0, 0)
+        {
+        }
+
+        CmsgReadItem(int8_t srcContainerSlot, uint8_t srcSlot) :
+            ManagedPacket(CMSG_READ_ITEM, 2),
+            srcContainerSlot(srcContainerSlot),
+            srcSlot(srcSlot)
+        {
+        }
+
+    protected:
+
+        bool internalSerialise(WorldPacket& packet) override
+        {
+            return false;
+        }
+
+        bool internalDeserialise(WorldPacket& packet) override
+        {
+            packet >> srcContainerSlot >> srcSlot;
+            return true;
+        }
+    };
+}}
