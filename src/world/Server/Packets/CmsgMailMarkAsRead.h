2eb04c3556a9401f7ff0640dd9801d919cc0e807
Zyres <mr@eichbuehlerhof.de>
2018-09-21 11:25:31 +0200

    Serialise CMSG_MAIL_MARK_AS_READ

---
+++ src/world/Server/Packets/CmsgMailMarkAsRead.h
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#pragma once
+
+#include <cstdint>
+
+#include "ManagedPacket.h"
+#include "WorldPacket.h"
+
+namespace AscEmu { namespace Packets
+{
+    class CmsgMailMarkAsRead : public ManagedPacket
+    {
+    public:
+        uint64_t gobjGuid;
+        uint32_t messageId;
+
+        CmsgMailMarkAsRead() : CmsgMailMarkAsRead(0, 0)
+        {
+        }
+
+        CmsgMailMarkAsRead(uint64_t gobjGuid, uint32_t messageId) :
+            ManagedPacket(CMSG_SET_LFG_COMMENT, 12),
+            gobjGuid(gobjGuid),
+            messageId(messageId)
+        {
+        }
+
+    protected:
+        bool internalSerialise(WorldPacket& packet) override
+        {
+            return false;
+        }
+
+        bool internalDeserialise(WorldPacket& packet) override
+        {
+            packet >> gobjGuid >> messageId;
+            return true;
+        }
+    };
+}}
