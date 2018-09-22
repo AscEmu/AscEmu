4332b19873a7c3f37b72282c0fe1ffd1a3fd134d
Zyres <mr@eichbuehlerhof.de>
2018-09-22 10:31:30 +0200

    Added File for serialised SMSG_ITEM_TEXT_QUERY_RESPONSE packet

---
+++ src/world/Server/Packets/SmsgItemTextQueryResponse.h
@@ -0,0 +1,49 @@
+/*
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
+
+#pragma once
+#include <cstdint>
+
+#include "ManagedPacket.h"
+
+namespace AscEmu { namespace Packets
+{
+    class SmsgItemTextQueryResponse : public ManagedPacket
+    {
+    public:
+        uint8_t result;
+        uint64_t guid;
+        std::string text;
+
+        SmsgItemTextQueryResponse() : SmsgItemTextQueryResponse(0, 0, "")
+        {
+        }
+
+        SmsgItemTextQueryResponse(uint8_t result, uint64_t guid, std::string text) :
+            ManagedPacket(SMSG_ITEM_TEXT_QUERY_RESPONSE, 0),
+            result(result),
+            guid(guid),
+            text(text)
+        {
+        }
+
+    protected:
+        size_t expectedSize() const override { return 1 + 8 + text.size(); }
+
+        bool internalSerialise(WorldPacket& packet) override
+        {
+            packet << result;
+            if (!result)
+                packet << guid << text;
+
+            return true;
+        }
+
+        bool internalDeserialise(WorldPacket& packet) override
+        {
+            return false;
+        }
+    };
+}}
