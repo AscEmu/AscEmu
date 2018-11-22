0b2fea88361a3901e11abd4f97b1328989153f06
schnek <tolstikov.misha@gmail.com>
2018-11-22 22:26:34 +0300

    FIXED: Removed extra spaces

--- src/tools/ToolsWotLK/map_extractor/adt.h
+++ src/tools/ToolsWotLK/map_extractor/adt.h
@@ -10,11 +10,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef ADT_H
@@ -277,10 +277,10 @@ public:
 
     uint32 pad;
     uint32 offsMCIN;           // MCIN
-    uint32 offsTex;               // MTEX
-    uint32 offsModels;           // MMDX
-    uint32 offsModelsIds;       // MMID
-    uint32 offsMapObejcts;       // MWMO
+    uint32 offsTex;            // MTEX
+    uint32 offsModels;         // MMDX
+    uint32 offsModelsIds;      // MMID
+    uint32 offsMapObejcts;     // MWMO
     uint32 offsMapObejctsIds;  // MWID
     uint32 offsDoodsDef;       // MDDF
     uint32 offsObjectsDef;     // MODF
