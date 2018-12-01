2cbe40b67faa4080958d588ccb0182e0086a5387
Zyres <mr@eichbuehlerhof.de>
2018-12-01 12:45:46 +0100

    Rewrite Simmon class

--- src/world/Units/Summons/Summon.cpp
+++ src/world/Units/Summons/Summon.cpp
@@ -1,140 +1,127 @@
 /*
- * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2005-2007 Ascent Team
- * Copyright (C) 2008-2011 <http://www.ArcEmu.org/>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
 
 #include "../../StdAfx.h"
 #include "Units/Creatures/Creature.h"
 #include "Units/Summons/Summon.h"
 
-// MIT START
-Object* Summon::getPlayerOwner()
+Summon::Summon(uint64_t guid) : Creature(guid), m_unitOwner(nullptr), m_summonSlot(-1) {}
+
+Summon::~Summon() {}
+
+void Summon::Load(CreatureProperties const* creatureProperties, Unit* unitOwner, LocationVector& position, uint32_t spellId, int32_t summonSlot)
 {
-    if (owner)
-        if (owner->isPlayer())
-            return owner;
+    ARCEMU_ASSERT(unitOwner != nullptr);
 
-    return nullptr;
-}
+    Creature::Load(creatureProperties, position.x, position.y, position.z, position.o);
 
-// MIT END
-Summon::Summon(uint64 GUID) : Creature(GUID)
-{
-    summonslot = -1;
-    owner = NULL;
-}
+    SetFaction(unitOwner->getFactionTemplate());
+    Phase(PHASE_SET, unitOwner->GetPhase());
+    SetZoneId(unitOwner->GetZoneId());
+    setCreatedBySpellId(spellId);
+    this->m_summonSlot = summonSlot;
 
-Summon::~Summon()
-{}
-
-void Summon::Load(CreatureProperties const* properties_, Unit* pOwner, LocationVector & position, uint32 spellid, int32 pSummonslot)
-{
-    ARCEMU_ASSERT(pOwner != nullptr);
-
-    Creature::Load(properties_, position.x, position.y, position.z, position.o);
-
-    SetFaction(pOwner->getFactionTemplate());
-    Phase(PHASE_SET, pOwner->GetPhase());
-    SetZoneId(pOwner->GetZoneId());
-    setCreatedBySpellId(spellid);
-    this->summonslot = pSummonslot;
-
-    if (pOwner->isPvpFlagSet())
+    if (unitOwner->isPvpFlagSet())
         setPvpFlag();
     else
         removePvpFlag();
 
-    if (pOwner->isFfaPvpFlagSet())
+    if (unitOwner->isFfaPvpFlagSet())
         setFfaPvpFlag();
     else
         removeFfaPvpFlag();
 
-    if (pOwner->isSanctuaryFlagSet())
+    if (unitOwner->isSanctuaryFlagSet())
         setSanctuaryFlag();
     else
         removeSanctuaryFlag();
 
-    setCreatedByGuid(pOwner->getGuid());
+    setCreatedByGuid(unitOwner->getGuid());
 
-    if (pOwner->getSummonedByGuid() == 0)
-        setSummonedByGuid(pOwner->getGuid());
+    if (unitOwner->getSummonedByGuid() == 0)
+        setSummonedByGuid(unitOwner->getGuid());
     else
-        setSummonedByGuid(pOwner->getSummonedByGuid());
+        setSummonedByGuid(unitOwner->getSummonedByGuid());
 
-    this->owner = pOwner;
+    this->m_unitOwner = unitOwner;
 
-    if (pOwner->hasUnitFlags(UNIT_FLAG_PVP_ATTACKABLE))
+    if (unitOwner->hasUnitFlags(UNIT_FLAG_PVP_ATTACKABLE))
         addUnitFlags(UNIT_FLAG_PVP_ATTACKABLE);
-
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Override Object functions
 
 void Summon::OnPushToWorld()
 {
-    if (summonslot != -1)
-        owner->summonhandler.AddSummonToSlot(this, static_cast<uint8_t>(summonslot));
+    if (m_summonSlot != -1)
+        m_unitOwner->summonhandler.AddSummonToSlot(this, static_cast<uint8_t>(m_summonSlot));
     else
-        owner->summonhandler.AddSummon(this);
+        m_unitOwner->summonhandler.AddSummon(this);
 
     Creature::OnPushToWorld();
-
 }
 
 void Summon::OnPreRemoveFromWorld()
 {
-    if (owner == NULL)
+    if (m_unitOwner == nullptr)
         return;
 
     if (getCreatedBySpellId() != 0)
-        owner->RemoveAura(getCreatedBySpellId());
+        m_unitOwner->RemoveAura(getCreatedBySpellId());
 
-    if (summonslot != -1)
-        owner->summonhandler.RemoveSummonFromSlot(static_cast<uint8_t>(summonslot), false);
+    if (m_summonSlot != -1)
+        m_unitOwner->summonhandler.RemoveSummonFromSlot(static_cast<uint8_t>(m_summonSlot), false);
     else
-        owner->summonhandler.RemoveSummon(this);
+        m_unitOwner->summonhandler.RemoveSummon(this);
 
-    summonslot = -1;
-    owner = NULL;
+    m_summonSlot = -1;
+    m_unitOwner = nullptr;
 
     SendDestroyObject();
-
 }
 
-void Summon::Die(Unit* pAttacker, uint32 damage, uint32 spellid)
-{
-    if (owner->isTotem())
-        owner->Die(pAttacker, damage, spellid);
-
-    Creature::Die(pAttacker, damage, spellid);
-
-    if (summonslot != -1)
-        owner->summonhandler.RemoveSummonFromSlot(static_cast<uint8_t>(summonslot), false);
-    else
-        owner->summonhandler.RemoveSummon(this);
-
-    summonslot = -1;
-    owner = NULL;
-}
+bool Summon::isSummon() const { return true; }
 
 void Summon::onRemoveInRangeObject(Object* object)
 {
-
-    if ((owner != NULL) && (object->getGuid() == owner->getGuid()))
+    if (m_unitOwner != nullptr && object->getGuid() == m_unitOwner->getGuid())
         Despawn(1, 0);
 
     Creature::onRemoveInRangeObject(object);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Override Unit functions
+void Summon::Die(Unit* pAttacker, uint32 damage, uint32 spellid)
+{
+    if (m_unitOwner->isTotem())
+        m_unitOwner->Die(pAttacker, damage, spellid);
+
+    Creature::Die(pAttacker, damage, spellid);
+
+    if (m_summonSlot != -1)
+        m_unitOwner->summonhandler.RemoveSummonFromSlot(static_cast<uint8_t>(m_summonSlot), false);
+    else
+        m_unitOwner->summonhandler.RemoveSummon(this);
+
+    m_summonSlot = -1;
+    m_unitOwner = nullptr;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Misc
+bool Summon::isSummonedToSlot() const
+{
+    return m_summonSlot != -1;
+}
+
+Object* Summon::getPlayerOwner()
+{
+    if (m_unitOwner && m_unitOwner->isPlayer())
+        return m_unitOwner;
+
+    return nullptr;
 }
--- src/world/Units/Summons/Summon.h
+++ src/world/Units/Summons/Summon.h
@@ -1,74 +1,41 @@
 /*
- * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2005-2007 Ascent Team
- * Copyright (C) 2008-2011 <http://www.ArcEmu.org/>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
+Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
 
-#ifndef SUMMON_HPP_
-#define SUMMON_HPP_
+#pragma once
 
 #include "SummonDefines.hpp"
 #include "Units/Creatures/Creature.h"
 
-// Base class for Summoned creatures
 class Summon : public Creature
 {
-    // MIT START
+public:
+    Summon(uint64_t guid);
+    ~Summon();
+
+    virtual void Load(CreatureProperties const* creatureProperties, Unit* unitOwner, LocationVector& position, uint32_t spellId, int32_t summonSlot);
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // Override Object functions
+    void OnPushToWorld() override;
+    void OnPreRemoveFromWorld() override;
+    bool isSummon() const override;
+    void onRemoveInRangeObject(Object* object) override;
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // Override Unit functions
+    void Die(Unit* pAttacker, uint32 damage, uint32 spellid) override;
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // Misc
 private:
-    Unit* owner;
+    int32_t m_summonSlot;
+    Unit* m_unitOwner;
 
 public:
-    Unit* getUnitOwner() { return owner; }
+    bool isSummonedToSlot() const;
+
+    Unit* getUnitOwner() const { return m_unitOwner; }
     Object* getPlayerOwner() override;
-
-    // MIT END
-
-        Summon(uint64 GUID);
-        ~Summon();
-
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //virtual void Load(CreatureProto *proto, Unit* owner, LocationVector &position, uint32 spellid, int32 summonslot);
-        //  Loads and sets up the basic properties of the summoned creature, like displayid, health, powers (mana and co.)
-        //
-        //Parameter(s)
-        //  CreatureProto *proto      -   Reference to the CreatureProto structure that belongs to this creature (basically a template)
-        //  Unit* owner               -   Owner unit of this Creature, usually the unit that summoned it, or the unit that summoned the summoner
-        //  LocationVector &position  -   Position of the summoned creature in the game world
-        //  uint32 spellid            -   The spell that summoned this creature
-        //  int32 summonslot          -   The summon slot of the creature, -1 means no slot
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        virtual void Load(CreatureProperties const* properties_, Unit* owner, LocationVector & position, uint32 spellid, int32 summonslot);
-        void OnPushToWorld();
-        void OnPreRemoveFromWorld();
-        bool SummonedToSlot()
-        {
-            if (summonslot != -1)
-                return true;
-            return false;
-        }
-        bool isSummon() const override { return true; }
-
-        
-        void Die(Unit* pAttacker, uint32 damage, uint32 spellid);
-        void onRemoveInRangeObject(Object* object);
-
-    private:
-
-        int32 summonslot;  // Summon slot of the creature in the owner's summonhandler, -1 means no slot
 };
-
-#endif      // SUMMON_HPP_
