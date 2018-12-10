90b2bb34c344bf1c20a898fa42ea713388778889
schnek <tolstikov.misha@gmail.com>
2018-12-10 23:45:44 +0300

    Maps/Warnings: Reduce the amount of warnings/err...

--- src/tools/ToolsCata/vmap_tools/vmap4_extractor/adtfile.cpp
+++ src/tools/ToolsCata/vmap_tools/vmap4_extractor/adtfile.cpp
@@ -214,7 +214,7 @@ bool ADTFile::init(uint32 map_num, uint32 tileX, uint32 tileY)
         }
 
         //======================
-        ADT.seek(nextpos);
+        ADT.seek(static_cast<int>(nextpos));
     }
 
     ADT.close();
