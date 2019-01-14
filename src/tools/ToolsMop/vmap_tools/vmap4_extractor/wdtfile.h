3150ffa529a3802c8ff6cd4b4c25adad797f4429
Zyres <mr@eichbuehlerhof.de>
2019-01-14 08:58:18 +0100

    Mop: Copy Cata Tool and include StormLib if Cata or Mop

---
+++ src/tools/ToolsMop/vmap_tools/vmap4_extractor/wdtfile.h
@@ -0,0 +1,46 @@
+/*
+ * AscEmu Framework based on ArcEmu MMORPG Server
+ * Copyright (c) 2014-2019 AscEmu Team <http://www.ascemu.org>
+ * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WDTFILE_H
+#define WDTFILE_H
+
+#include "mpqfile.h"
+#include "wmo.h"
+#include <string>
+#include "stdlib.h"
+
+class ADTFile;
+
+class WDTFile
+{
+private:
+    MPQFile WDT;
+    string filename;
+public:
+    WDTFile(char* file_name, char* file_name1);
+    ~WDTFile(void);
+    bool init(char* map_id, unsigned int mapID);
+
+    string* gWmoInstansName;
+    int gnWMO;
+
+    ADTFile* GetMap(int x, int z);
+};
+
+#endif  //WDTFILE_H
