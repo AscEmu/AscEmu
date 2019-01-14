c7b992d83fdaed5f8a92e8d590d7260ed321e1ee
schnek <tolstikov.misha@gmail.com>
2019-01-14 22:33:58 +0300

    typo..

--- src/tools/ToolsCata/mmaps_generator/MapBuilder.cpp
+++ src/tools/ToolsCata/mmaps_generator/MapBuilder.cpp
@@ -1,6 +1,6 @@
 /*
  * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2014-2018 AscEmu Team <http://www.ascemu.org>
+ * Copyright (C) 2014-2019 AscEmu Team <http://www.ascemu.org>
  * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -122,7 +122,7 @@ namespace MMAP
             getDirContents(files, "vmaps", filter);
             for (uint32 i = 0; i < files.size(); ++i)
             {
-                tileX = uint32(atoi(files[i].substr(7,2).c_str()));
+                tileX = uint32(atoi(files[i].substr(8, 2).c_str()));
                 tileY = uint32(atoi(files[i].substr(5, 2).c_str()));
                 tileID = StaticMapTree::packTileID(tileY, tileX);
 
