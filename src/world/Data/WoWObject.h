47a89b7499139981913b4465c56d3c30fc8ae6b5
Zyres <mr@eichbuehlerhof.de>
2019-01-14 06:32:44 +0100

    MOP: add updated object structure

--- src/world/Data/WoWObject.h
+++ src/world/Data/WoWObject.h
@@ -19,6 +19,7 @@ This file is released under the MIT license. See README-MIT for more information
 #include "WorldConf.h"
 
 #pragma pack(push, 1)
+#if VERSION_STRING < Cata
 struct WoWObject
 {
     union
@@ -50,4 +51,65 @@ struct WoWObject
         *(reinterpret_cast<uint32_t*>(&guid) + 1) = val;
     }
 };
+#elif VERSION_STRING == Cata
+struct WoWObject
+{
+    union
+    {
+        struct
+        {
+            uint32_t low;
+            uint32_t high;
+        } guid_parts;
+        uint64_t guid;
+    };
+
+    uint64_t data;
+
+    uint32_t type;
+    uint32_t entry;
+    float scale_x;
+    uint32_t padding_object;
+
+    void setLowGuid(uint32_t val)
+    {
+        *reinterpret_cast<uint32_t*>(&guid) = val;
+    }
+
+    void setHighGuid(uint32_t val)
+    {
+        *(reinterpret_cast<uint32_t*>(&guid) + 1) = val;
+    }
+};
+#elif VERSION_STRING == Mop
+struct WoWObject
+{
+    union
+    {
+        struct
+        {
+            uint32_t low;
+            uint32_t high;
+        } guid_parts;
+        uint64_t guid;
+    };
+
+    uint64_t data;
+
+    uint32_t type;
+    uint32_t entry;
+    uint32_t dynamic_flags;
+    float scale_x;
+
+    void setLowGuid(uint32_t val)
+    {
+        *reinterpret_cast<uint32_t*>(&guid) = val;
+    }
+
+    void setHighGuid(uint32_t val)
+    {
+        *(reinterpret_cast<uint32_t*>(&guid) + 1) = val;
+    }
+};
+#endif
 #pragma pack(pop)
