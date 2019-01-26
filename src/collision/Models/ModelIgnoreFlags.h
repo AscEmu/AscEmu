d65a2872fac3137cc41d8700d05eccb1792bb274
schnek <tolstikov.misha@gmail.com>
2019-01-26 15:24:52 +0300

    Style: AE Style apply project rules (typo)

--- src/collision/Models/ModelIgnoreFlags.h
+++ src/collision/Models/ModelIgnoreFlags.h
@@ -1,37 +1,22 @@
 /*
- * AscEmu Framework based on ArcEmu MMORPG Server
- * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+Copyright (c) 2014-2019 AscEmu Team <http://www.ascemu.org>
+This file is released under the MIT license. See README-MIT for more information.
+*/
 
-#ifndef ModelIgnoreFlags_h__
-#define ModelIgnoreFlags_h__
+#pragma once
 
-
-#include "Common.hpp" // #include "Define.h"
+#include "Common.hpp"
 
 namespace VMAP
 {
-enum class ModelIgnoreFlags : uint32
-{
-    Nothing = 0x00,
-    M2      = 0x01
-};
+    enum class ModelIgnoreFlags : uint32_t
+    {
+        Nothing = 0x00,
+        M2      = 0x01
+    };
 
-inline ModelIgnoreFlags operator&(ModelIgnoreFlags left, ModelIgnoreFlags right) { return ModelIgnoreFlags(uint32(left) & uint32(right)); }
+    inline ModelIgnoreFlags operator&(ModelIgnoreFlags left, ModelIgnoreFlags right)
+    {
+        return ModelIgnoreFlags(uint32_t(left) & uint32_t(right));
+    }
 }
-
-
-#endif // ModelIgnoreFlags_h__
