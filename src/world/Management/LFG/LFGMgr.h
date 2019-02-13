17fba60bbf91ca6a3c07ae692529aa7a0214753c
schnek <tolstikov.misha@gmail.com>
2019-02-13 23:26:01 +0300

    Added data for MAX_DUNGEONS

--- src/world/Management/LFG/LFGMgr.h
+++ src/world/Management/LFG/LFGMgr.h
@@ -11,9 +11,35 @@ This file is released under the MIT license. See README-MIT for more information
 #include "../../shared/Singleton.h"
 #include "Server/EventableObject.h"
 
-#define MAX_DUNGEONS 294+1          // check max entry's +1 on lfgdungeons.dbc
 #define MAX_LFG_QUEUE_ID 3
-#define LFG_MATCH_TIMEOUT 30        // in seconds
+#define LFG_MATCH_TIMEOUT 30 // in seconds
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// MAX_DUNGEONS
+//
+// \param Max dungeons (Max entry's +1 on lfgdungeons.dbc)
+//
+// Vanilla = 165+1
+// The Burning Crusade = 201+1
+// Wrath of the Lich King = 294+1
+// Cataclysm = 448+1
+// Mists of Pandaria = untested+1
+// Warlords of Draenor = untested+1
+// Legion = untested+1
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#if VERSION_STRING == Classic
+    #define MAX_DUNGEONS 166    // Classic WoW (untested)
+#elif VERSION_STRING == TBC
+    #define MAX_DUNGEONS 202    // The Burning Crusade
+#elif VERSION_STRING == WotLK
+    #define MAX_DUNGEONS 295    // Wrath of the Lich King
+#elif VERSION_STRING == Cata
+    #define MAX_DUNGEONS 449    // Cataclysm
+#elif VERSION_STRING == Mop
+    #define MAX_DUNGEONS 449    //  Mists of Pandaria (untested)
+#endif
 
 class LfgGroupData;
 class LfgPlayerData;
