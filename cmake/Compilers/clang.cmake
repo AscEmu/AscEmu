2ffc2794929ea3f2f201481f11deda7c80fa1fb9
Zyres <mr@eichbuehlerhof.de>
2019-06-22 20:35:13 +0200

    Update cmake compiler clang

--- cmake/Compilers/clang.cmake
+++ cmake/Compilers/clang.cmake
@@ -1,10 +1,10 @@
 # Copyright (c) 2014-2019 AscEmu Team <http://www.ascemu.org>
 
 include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX11)
+CHECK_CXX_COMPILER_FLAG("-std=c++17" COMPILER_SUPPORTS_CXX17)
 
-if(NOT COMPILER_SUPPORTS_CXX11)
-    message(FATAL_ERROR "AscEmu requires at least Clang 3.8! Current version ${CMAKE_CXX_COMPILER} does not support c++14 feature")
+if(NOT COMPILER_SUPPORTS_CXX17)
+    message(FATAL_ERROR "AscEmu requires at least Clang 7.0! Current version ${CMAKE_CXX_COMPILER} does not support c++17 feature")
 endif()
 
 message(STATUS "Applying settings for ${CMAKE_CXX_COMPILER}")
@@ -12,7 +12,7 @@ add_definitions(-DHAS_CXX0X)
 
 # apply base flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -std=c11")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -std=c++14")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -std=c++17")
 
 if (IS_64BIT)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
