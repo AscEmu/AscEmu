5d49579d11d1b1bff6f1130b8bb0cc7a585e80f1
Zyres <mr@eichbuehlerhof.de>
2020-01-03 07:30:58 +0100

    Resolved CID 351689 (#1 of 1): Invalid type in argument to printf format specifier
    invalid_type: Argument achievement->ID to format specifier %lu was expected to have type unsigned long but has type unsigned int

--- src/world/Management/AchievementMgr.cpp
+++ src/world/Management/AchievementMgr.cpp
@@ -1936,7 +1936,7 @@ bool AchievementMgr::GMCompleteCriteria(WorldSession* gmSession, int32_t criteri
     if (achievement->flags & ACHIEVEMENT_FLAG_COUNTER)
     {
         // can't complete this type of achivement (counter)
-        gmSession->SystemMessage("Referred achievement (%lu) |Hachievement:%lu:" I64FMT ":0:0:0:-1:0:0:0:0|h[%s]|h is a counter and cannot be completed.",
+        gmSession->SystemMessage("Referred achievement (%u) |Hachievement:%u:" I64FMT ":0:0:0:-1:0:0:0:0|h[%s]|h is a counter and cannot be completed.",
                                  achievement->ID, achievement->ID, gmSession->GetPlayer()->getGuid(), achievement->name);
         return false;
     }
