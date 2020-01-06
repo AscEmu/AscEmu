2b7beb2d28f8af4dd375372f4ad67f3cc16a3ecd
Zyres <mr@eichbuehlerhof.de>
2020-01-06 10:37:27 +0100

    Resolved CID 351835 (Unchecked dynamic_cast)
    var_deref_model: Passing null pointer player to _ZNK6Player7getNameB5cxx11Ev, which dereferences it.

--- src/world/Chat/Commands/ModifyCommands.cpp
+++ src/world/Chat/Commands/ModifyCommands.cpp
@@ -787,11 +787,13 @@ bool ChatHandler::HandleModifyFlags(const char* args, WorldSession* session)
 
     if (unitTarget->isPlayer())
     {
-        const auto player = dynamic_cast<Player*>(unitTarget);
-        sGMLog.writefromsession(session, "used modify flags from %u to %u on %s (%u)", oldUnitFlags, value, player->getName().c_str(), player->getGuidLow());
+        if (const auto player = dynamic_cast<Player*>(unitTarget))
+        {
+            sGMLog.writefromsession(session, "used modify flags from %u to %u on %s (%u)", oldUnitFlags, value, player->getName().c_str(), player->getGuidLow());
 
-        BlueSystemMessage(session, "You modify the flags of %s from %u to %u.", player->getName().c_str(), oldUnitFlags, value);
-        GreenSystemMessage(player->GetSession(), "%s modify your flags from %u to %u.", session->GetPlayer()->getName().c_str(), oldUnitFlags, value);
+            BlueSystemMessage(session, "You modify the flags of %s from %u to %u.", player->getName().c_str(), oldUnitFlags, value);
+            GreenSystemMessage(player->GetSession(), "%s modify your flags from %u to %u.", session->GetPlayer()->getName().c_str(), oldUnitFlags, value);
+        }
     }
     else if (unitTarget->isCreature())
     {
