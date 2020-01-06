eb222265606fbf429444ad5e593152d3ac95e580
Zyres <mr@eichbuehlerhof.de>
2020-01-06 17:45:00 +0100

    Resolved CID 351546 (Out-of-bounds write)
    overrun-local: Overrunning array this->Effect of 3 4-byte elements at element index 3 (byte offset 15) using index idx (which evaluates to 3).

--- src/world/Spell/SpellInfo.hpp
+++ src/world/Spell/SpellInfo.hpp
@@ -440,7 +440,12 @@ private:
 
     void setEffect(uint32_t effectId, uint8_t idx)                          // used in HackFixes.cpp / ObjectMgr.cpp
     {
-        ARCEMU_ASSERT(idx < MAX_SPELL_EFFECTS);
+        if (idx >= MAX_SPELL_EFFECTS)
+        {
+            LogError("Effect index id %u is invalid!", idx);
+            return;
+        }
+
         Effect[idx] = effectId;
     }
 
