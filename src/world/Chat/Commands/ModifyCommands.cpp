bda779f0ce54c8ee47e52bbeb7dc8e4b9f237e1b
Zyres <mr@eichbuehlerhof.de>
2020-01-06 12:17:23 +0100

    Resolved CID 351753 (Unchecked dynamic_cast)
    var_deref_model: Passing null pointer player to _ZNK6Player7getNameB5cxx11Ev, which dereferences it.

--- src/world/Chat/Commands/ModifyCommands.cpp
+++ src/world/Chat/Commands/ModifyCommands.cpp
@@ -485,11 +485,13 @@ bool ChatHandler::HandleModifyFrost(const char* args, WorldSession* session)
 
     if (unitTarget->isPlayer())
     {
-        const auto player = dynamic_cast<Player*>(unitTarget);
-        sGMLog.writefromsession(session, "used modify frost from %u to %u on %s (%u)", oldFrost, value, player->getName().c_str(), player->getGuidLow());
+        if (const auto player = dynamic_cast<Player*>(unitTarget))
+        {
+            sGMLog.writefromsession(session, "used modify frost from %u to %u on %s (%u)", oldFrost, value, player->getName().c_str(), player->getGuidLow());
 
-        BlueSystemMessage(session, "You modify the frost of %s from %u to %u.", player->getName().c_str(), oldFrost, value);
-        GreenSystemMessage(player->GetSession(), "%s modify your frost from %u to %u.", session->GetPlayer()->getName().c_str(), oldFrost, value);
+            BlueSystemMessage(session, "You modify the frost of %s from %u to %u.", player->getName().c_str(), oldFrost, value);
+            GreenSystemMessage(player->GetSession(), "%s modify your frost from %u to %u.", session->GetPlayer()->getName().c_str(), oldFrost, value);
+        }
     }
     else if (unitTarget->isCreature())
     {
