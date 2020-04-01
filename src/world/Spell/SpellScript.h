ab271de12d308e38dcdeb81b3310afdd55b746d1
Andrius Peleckas <32540208+sanctum32@users.noreply.github.com>
2020-04-01 09:27:46 +0300

    Script/Spells: implemented new scriptable events (#737)
    * Script/Spells: implemented new scriptable events - beforeSpellEffect (script event is executed before spell handler, it also can prevent default spell effect) and afterSpellEffect (event called after spell effect handling)
    
    * Script/Spells: additions to previous commit - moved initial handling to ScriptMgr class instead
    
    * Script/Spells: removed useless cast made in previous commits

--- src/world/Spell/SpellScript.h
+++ src/world/Spell/SpellScript.h
@@ -11,6 +11,14 @@ This file is released under the MIT license. See README-MIT for more information
 #include "CommonTypes.hpp"
 
 class Spell;
+
+enum class SpellScriptExecuteState : uint8_t
+{
+    EXECUTE_NOT_HANDLED = 0,    // Spell script is not found or not handled
+    EXECUTE_OK,                 // Spell script is executed
+    EXECUTE_PREVENT,            // Spell script is executed but prevent default effect
+};
+
 class SERVER_DECL SpellScript
 {
 public:
@@ -19,7 +27,7 @@ public:
     virtual ~SpellScript() {}
 
     // Called at the end of spell check cast function
-    virtual SpellCastResult onCanCast(Spell* /*spell*/, uint32_t* /*parameter1*/, uint32_t* /*parameter2*/) { return SPELL_CAST_SUCCESS; }
+    virtual SpellCastResult onCanCast(Spell* /*spell*/, uint32_t* /*parameter1*/, uint32_t* /*parameter2*/) const { return SPELL_CAST_SUCCESS; }
     // Called when cast bar is sent to client (NOT called for instant spells)
     virtual void doAtStartCasting(Spell* /*spell*/) {}
     // Called after spell targets for this effect have been initialized
@@ -28,4 +36,8 @@ public:
     virtual void doBeforeEffectHit(Spell* /*spell*/, uint8_t /*effectIndex*/) {}
     // Called after target missed/resisted the spell
     virtual void doAfterSpellMissed(Spell* /*spell*/, Unit* /*unitTarget*/) {}
+    // Called before spell effect type handling
+    virtual SpellScriptExecuteState beforeSpellEffect(Spell* /*spell*/, uint32_t /*effectType*/, uint8_t /*effectId*/) const { return SpellScriptExecuteState::EXECUTE_NOT_HANDLED; }
+    // Called after spell effect type handling
+    virtual void afterSpellEffect(Spell* /*spell*/, uint32_t /*effectType*/, uint8_t /*effectId*/) {}
 };
