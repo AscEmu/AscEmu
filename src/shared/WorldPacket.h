1a8beff96dcec7601baa447364fe972d080077d5
Zyres <mr@eichbuehlerhof.de>
2020-06-26 01:46:40 +0200

    Remove version specific code from WorldPacket

--- src/shared/WorldPacket.h
+++ src/shared/WorldPacket.h
@@ -23,12 +23,9 @@
 
 #include "CommonTypes.hpp"
 #include "ByteBuffer.h"
-#include "StackBuffer.h"
-#include "../world/Server/Opcodes.hpp"
 
 class SERVER_DECL WorldPacket : public ByteBuffer
 {
-#if VERSION_STRING != Mop
     public:
         __inline WorldPacket() : ByteBuffer(0), m_opcode(0) { }
         __inline WorldPacket(uint16 opcode, size_t res) : ByteBuffer(res), m_opcode(opcode) {}
@@ -48,45 +45,7 @@ class SERVER_DECL WorldPacket : public ByteBuffer
 
     protected:
         uint16 m_opcode;
-#else
-public:
-    __inline WorldPacket() : ByteBuffer(0), m_opcode(MSG_NULL_ACTION) { }
-    __inline WorldPacket(uint32 opcode, size_t res) : ByteBuffer(res), m_opcode(opcode) {}
-    __inline WorldPacket(size_t res) : ByteBuffer(res), m_opcode(0) { }
-    __inline WorldPacket(const WorldPacket & packet) : ByteBuffer(packet), m_opcode(packet.m_opcode) {}
 
-    //! Clear packet and set opcode all in one mighty blow
-    __inline void Initialize(uint16 opcode, size_t newres = 200)
-    {
-        clear();
-        _storage.reserve(newres);
-        m_opcode = opcode;
-    }
-
-    __inline uint32 GetOpcode() const { return m_opcode; }
-    __inline void SetOpcode(uint32 opcode) { m_opcode = opcode; }
-
-protected:
-    uint16 m_opcode;
-#endif
-};
-
-template<uint32 Size>
-class SERVER_DECL StackWorldPacket : public StackBuffer<Size>
-{
-        uint16 m_opcode;
-    public:
-        __inline StackWorldPacket(uint16 opcode) : StackBuffer<Size>(), m_opcode(opcode) { }
-
-        //! Clear packet and set opcode all in one mighty blow
-        __inline void Initialize(uint16 opcode)
-        {
-            StackBuffer<Size>::Clear();
-            m_opcode = opcode;
-        }
-
-        uint16 GetOpcode() { return m_opcode; }
-        __inline void SetOpcode(uint16 opcode) { m_opcode = opcode; }
 };
 
 #endif
