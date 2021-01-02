1b6416177c1142baddcf9b9ac97bb132b5401949
Appled <346848+Appled@users.noreply.github.com>
2021-01-02 16:46:15 +0200

    Some codestyle cleanup

--- src/world/Movement/Spline/New/MovementPacketBuilder.h
+++ src/world/Movement/Spline/New/MovementPacketBuilder.h
@@ -3,12 +3,12 @@ Copyright (c) 2014-2021 AscEmu Team <http://www.ascemu.org>
 This file is released under the MIT license. See README-MIT for more information.
 */
 
-#ifndef PACKET_BUILDER_H
-#define PACKET_BUILDER_H
+#pragma once
 
 #include "CommonTypes.hpp"
 
 class ByteBuffer;
+
 namespace G3D
 {
     class Vector3;
@@ -22,9 +22,8 @@ namespace MovementNew
         static void WriteCommonMonsterMovePart(MoveSpline const& mov, ByteBuffer& data);
     public:
         static void WriteMonsterMove(MoveSpline const& mov, ByteBuffer& data);
-        static void WriteStopMovement(G3D::Vector3 const& loc, uint32 splineId, ByteBuffer& data);
+        static void WriteStopMovement(G3D::Vector3 const& loc, uint32_t splineId, ByteBuffer& data);
         static void WriteCreate(MoveSpline const& mov, ByteBuffer& data);
         static void WriteSplineSync(MoveSpline const& mov, ByteBuffer& data);
     };
 }
-#endif // PACKET_BUILDER_H
