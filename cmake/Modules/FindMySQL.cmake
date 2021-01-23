36a60d71595bb77df656308c8a7401a587898586
Zyres <mr@eichbuehlerhof.de>
2021-01-23 12:42:59 +0100

    Remove delivered mysql source

--- cmake/Modules/FindMySQL.cmake
+++ cmake/Modules/FindMySQL.cmake
@@ -4,47 +4,45 @@
 #  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
 #  MYSQL_LIBRARIES   - List of libraries when using MySQL.
 #  MYSQL_FOUND       - True if MySQL found.
-#  From http://www.vtk.org/Wiki/CMakeUserFindMySQL
 
-IF (MYSQL_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(MYSQL_FIND_QUIETLY TRUE)
-ENDIF (MYSQL_INCLUDE_DIR)
+find_path(MYSQL_INCLUDE_DIR
+    NAMES "mysql.h"
+    PATHS "$ENV{ProgramFiles}/MySQL/*/include"
+            "$ENV{ProgramW6432}/MySQL/*/include"
+            "$ENV{SystemDrive}/MySQL/*/include"
+            "/usr/include/mysql"
+            "/usr/local/include/mysql"
+            "/usr/mysql/include/mysql")
+	
+find_library(MYSQL_LIBRARY
+    NAMES "libmysql" "libmysql_r"
+    PATHS "$ENV{ProgramFiles}/MySQL/*/lib"
+            "$ENV{ProgramW6432}/MySQL/*/lib"
+            "$ENV{SystemDrive}/MySQL/*/lib"
+            "/lib/mysql"
+            "/lib64/mysql"
+            "/usr/lib/mysql"
+            "/usr/lib64/mysql"
+            "/usr/local/lib/mysql"
+            "/usr/local/lib64/mysql"
+            "/usr/mysql/lib/mysql"
+            "/usr/mysql/lib64/mysql")
 
-FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
-  /usr/local/include/mysql
-  /usr/include/mysql
-  /usr/local/mysql/include
-  /opt/local/include/mysql5/mysql
-)
 
-SET(MYSQL_NAMES mysqlclient mysqlclient_r)
-FIND_LIBRARY(MYSQL_LIBRARY
-  NAMES ${MYSQL_NAMES}
-  PATHS /usr/lib /usr/lib/mysql /usr/local/lib /usr/local/mysql/lib /usr/local/lib/mysql /opt/local/lib/mysql5/mysql
-)
-
-IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND TRUE)
-  SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
-ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND FALSE)
-  SET( MYSQL_LIBRARIES )
-ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-
-IF (MYSQL_FOUND)
-  IF (NOT MYSQL_FIND_QUIETLY)
-    MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
-  ENDIF (NOT MYSQL_FIND_QUIETLY)
-ELSE (MYSQL_FOUND)
-  IF (MYSQL_FIND_REQUIRED)
-    MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
-    MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
-  ENDIF (MYSQL_FIND_REQUIRED)
-ENDIF (MYSQL_FOUND)
-
-MARK_AS_ADVANCED(
-  MYSQL_LIBRARY
-  MYSQL_INCLUDE_DIR
-  )
-
+if (MYSQL_LIBRARY)
+    if (MYSQL_INCLUDE_DIR)
+        set(MYSQL_FOUND 1)
+        message(STATUS "Found MySQL library: ${MYSQL_LIBRARY}")
+        message(STATUS "Found MySQL headers: ${MYSQL_INCLUDE_DIR}")
+        if (WIN32)
+            set(MYSQL_DLL ${MYSQL_LIBRARY})
+            STRING(REPLACE ".lib" ".dll" MYSQL_DLL ${MYSQL_DLL})
+            message(STATUS "Found MySQL dll: ${MYSQL_DLL}")
+        endif (WIN32)
+    else (MYSQL_INCLUDE_DIR)
+        message(FATAL_ERROR "Could not find MySQL headers! Please install the development libraries and headers")
+    endif (MYSQL_INCLUDE_DIR)
+    mark_as_advanced(MYSQL_FOUND MYSQL_LIBRARY)
+else (MYSQL_LIBRARY)
+    message(FATAL_ERROR "Could not find the MySQL libraries! Please install the development libraries and headers")
+endif (MYSQL_LIBRARY)
--- cmake/Systems/Windows.cmake
+++ cmake/Systems/Windows.cmake
@@ -9,28 +9,25 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     set(CMAKE_INSTALL_PREFIX "C:/AscEmu" CACHE PATH "Install path prefix" FORCE)
 endif()
 
+include(${CMAKE_SOURCE_DIR}/cmake/Modules/FindMySQL.cmake)
+
 # set source paths for libraries
 if(IS_64BIT)
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(MYSQL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libmysql_debug_x64.lib CACHE INTERNAL "MYSQL libraries." FORCE)
         set(OPENSSL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libeay32_debug_x64.lib CACHE INTERNAL "OpenSSL libraries." FORCE)
     else()
-        set(MYSQL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libmysql_release_x64.lib CACHE INTERNAL "MYSQL libraries." FORCE)
         set(OPENSSL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libeay32_release_x64.lib CACHE INTERNAL "OpenSSL libraries." FORCE)
     endif()
 else()
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(MYSQL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libmysql_debug_win32.lib CACHE INTERNAL "MYSQL libraries." FORCE)
         set(OPENSSL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libeay32_debug_win32.lib CACHE INTERNAL "OpenSSL libraries." FORCE)
     else()
-        set(MYSQL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libmysql_release_win32.lib CACHE INTERNAL "MYSQL libraries." FORCE)
         set(OPENSSL_LIBRARIES ${CMAKE_SOURCE_DIR}/dep/lib/libeay32_release_win32.lib CACHE INTERNAL "OpenSSL libraries." FORCE)
     endif()
 endif()
 
 # set dependencies include dirs paths
 set(OPENSSL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/openssl CACHE INTERNAL "OpenSSL include dir." FORCE)
-set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysql CACHE INTERNAL "MYSQL include dir." FORCE)
 
 #Needed for socket stuff and crash handler
 set(EXTRA_LIBS 
@@ -48,7 +45,7 @@ endif()
 
 #Install libmysql.dll required for our core to run.
 set(INSTALLED_DEPENDENCIES
-    ${DEPENDENCY_DLLS}/libmysql.dll
+    ${MYSQL_DLL}
     ${DEPENDENCY_DLLS}/libeay32.dll
 )
 
