e7fb594e1c84c325f84d46934e45ade2e72d3907
Zyres <mr@eichbuehlerhof.de>
2021-04-24 14:31:18 +0200

    Remove redundant inline specifier

--- src/shared/WorldPacket.h
+++ src/shared/WorldPacket.h
@@ -11,21 +11,21 @@ This file is released under the MIT license. See README-MIT for more information
 class SERVER_DECL WorldPacket : public ByteBuffer
 {
 public:
-    inline WorldPacket() : ByteBuffer(0), m_opcode(0) { }
-    inline WorldPacket(uint16_t opcode, size_t res) : ByteBuffer(res), m_opcode(opcode) {}
-    inline WorldPacket(size_t res) : ByteBuffer(res), m_opcode(0) { }
-    inline WorldPacket(const WorldPacket & packet) : ByteBuffer(packet), m_opcode(packet.m_opcode) {}
+    WorldPacket() : ByteBuffer(0), m_opcode(0) {}
+    WorldPacket(uint16_t opcode, size_t res) : ByteBuffer(res), m_opcode(opcode) {}
+    WorldPacket(size_t res) : ByteBuffer(res), m_opcode(0) {}
+    WorldPacket(const WorldPacket & packet) : ByteBuffer(packet), m_opcode(packet.m_opcode) {}
 
     // Clear packet and set opcode all in one mighty blow
-    inline void Initialize(uint16_t opcode, size_t newres = 200)
+    void Initialize(uint16_t opcode, size_t newres = 200)
     {
         clear();
         _storage.reserve(newres);
         m_opcode = opcode;
     }
 
-    inline uint16_t GetOpcode() const { return m_opcode; }
-    inline void SetOpcode(uint16_t opcode) { m_opcode = opcode; }
+    uint16_t GetOpcode() const { return m_opcode; }
+    void SetOpcode(uint16_t opcode) { m_opcode = opcode; }
 
 protected:
     uint16_t m_opcode;
