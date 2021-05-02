71305cfc27057eb65f470072ad0375f323b74167
aaron02 <aaron.herrsche@hotmail.com>
2021-05-02 16:58:40 +0200

    Fix Build

--- src/scripts/ExtraScripts/Setup.h
+++ src/scripts/ExtraScripts/Setup.h
@@ -5,6 +5,8 @@ This file is released under the MIT license. See README-MIT for more information
 
 #pragma once
 
+#include "../world/Units/Creatures/Creature.h"
+
 class ScriptMgr;
 
 void SetupNeutralGuards(ScriptMgr* mgr);
