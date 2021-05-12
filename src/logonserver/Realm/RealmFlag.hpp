6ec03cde515cbb72ec2c2794178e383544c3c610
Adrian Weihs <adiweihs@yahoo.de>
2021-05-12 21:09:08 +0200

    fix

--- src/logonserver/Realm/RealmFlag.hpp
+++ src/logonserver/Realm/RealmFlag.hpp
@@ -15,7 +15,7 @@ namespace AscEmu::Realm
         OFFLINE = 0x02,
         SPECIFIC_BUILD = 0x04,     // client will show realm version in RealmList screen in form "RealmName (major.minor.revision.build)"
         UNKNOWN_1 = 0x08,
-        UNKOWN_2 = 0x10,
+        UNKNOWN_2 = 0x10,
         ///\todo Other emulators have REALM_FLAG_NEW_PLAYERS and REALM_FLAG_RECOMMENDED swaped.
         NEW_PLAYERS = 0x20,
         RECOMMENDED = 0x40,
