700a96f08fab613f2ee58edc43ad60aa8eb67832
Zyres <mr@eichbuehlerhof.de>
2021-07-03 16:51:01 +0200

    Use cmake included function instead of cotire

--- cmake/Modules/PCHSupport.cmake
+++ cmake/Modules/PCHSupport.cmake
@@ -1,13 +1,5 @@
-include(cotire)
-
 function(GEN_CXX_PCH TARGET_LIST PCH_HEADERS)
-  # iterate through targets
   foreach(TARGET_HEADER ${TARGET_LIST})
-    # unity builds disabled
-    set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_ADD_UNITY_BUILD OFF)
-
-    # prefix header
-    set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT ${PCH_HEADERS})
+    target_precompile_headers(${TARGET_HEADER} PRIVATE ${PCH_HEADERS})
   endforeach()
-  cotire(${TARGET_LIST})
 endfunction(GEN_CXX_PCH)
