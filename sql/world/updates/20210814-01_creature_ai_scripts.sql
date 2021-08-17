dcecceacbc43a028d2109d8f0a3ebc2142f2fcd9
Zyres <mr@eichbuehlerhof.de>
2021-08-17 19:34:10 +0200

    Fix multibuild logic. min/max_nuild has to be part of the unique key.
    Add default value to spell_type.
    Remove "NOT NULL" from comments (I guess comments are just for mysql devs, so it can be NULL

--- sql/world/updates/20210814-01_creature_ai_scripts.sql
+++ sql/world/updates/20210814-01_creature_ai_scripts.sql
@@ -3,6 +3,8 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `creature_ai_scripts`;
 CREATE TABLE `creature_ai_scripts` (
+  `min_build` int NOT NULL DEFAULT '12340',
+  `max_build` int NOT NULL DEFAULT '12340',
   `entry` int unsigned NOT NULL,
   `difficulty` tinyint unsigned NOT NULL DEFAULT '0',
   `phase` tinyint unsigned NOT NULL DEFAULT '0',
@@ -11,7 +13,7 @@ CREATE TABLE `creature_ai_scripts` (
   `maxCount` tinyint unsigned NOT NULL DEFAULT '0',
   `chance` float unsigned NOT NULL DEFAULT '1',
   `spell` int unsigned NOT NULL DEFAULT '0',
-  `spell_type` int NOT NULL,
+  `spell_type` int NOT NULL DEFAULT '0',
   `triggered` tinyint(1) NOT NULL DEFAULT '0',
   `target` tinyint NOT NULL DEFAULT '0',
   `cooldownMin` int NOT NULL DEFAULT '0',
@@ -20,10 +22,8 @@ CREATE TABLE `creature_ai_scripts` (
   `maxHealth` float NOT NULL DEFAULT '100',
   `textId` int unsigned NOT NULL DEFAULT '0',
   `misc1` int NOT NULL DEFAULT '0',
-  `comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `min_build` int NOT NULL DEFAULT '12340',
-  `max_build` int NOT NULL DEFAULT '12340',
-  UNIQUE KEY `entry` (`entry`,`difficulty`,`phase`,`spell`,`event`,`action`) USING BTREE
+  `comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  UNIQUE KEY `entry` (`min_build`,`max_build`,`entry`,`difficulty`,`phase`,`spell`,`event`,`action`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='AI System';
 
 REPLACE INTO `world_db_version` VALUES ('85', '20210814-01_creature_ai_scripts');
