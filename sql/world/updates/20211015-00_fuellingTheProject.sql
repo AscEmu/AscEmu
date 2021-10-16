b385c3fa9123e4dca4e3a1421a25ce8d34077f44
aaron02 <aaron.herrsche@hotmail.com>
2021-10-17 00:27:12 +0200

    sql

---
+++ sql/world/updates/20211015-00_fuellingTheProject.sql
@@ -0,0 +1,3 @@
+UPDATE `spelltargetconstraints` SET `TargetType`='2' WHERE (`SpellID`='45990') AND (`TargetType`='0') AND (`TargetID`='25781');
+
+INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES ('93', '20211015-00_fuellingTheProject');
