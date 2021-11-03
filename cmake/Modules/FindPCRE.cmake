994cedbae38f630dd441d256d5cdb0a9cd964192
schnek <tolstikov.misha@gmail.com>
2021-11-04 01:06:35 +0300

    Update FindPCRE.cmake (register)

--- cmake/Modules/FindPCRE.cmake
+++ cmake/Modules/FindPCRE.cmake
@@ -7,7 +7,7 @@
 
 if (PCRE_INCLUDE_DIRS)
     # Already in cache, be silent
-    SET(PCRE_FIND_QUIETLY TRUE)
+    set(PCRE_FIND_QUIETLY TRUE)
 endif (PCRE_INCLUDE_DIRS)
 
 find_path(PCRE_INCLUDE_DIR pcre.h)
@@ -18,7 +18,7 @@ find_library(PCRE_LIBRARY NAMES ${PCRE_NAMES} )
 # handle the QUIETLY and REQUIRED arguments and set PCRE_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCRE DEFAULT_MSG PCRE_LIBRARY PCRE_INCLUDE_DIR)
+find_package_handle_standard_args(PCRE DEFAULT_MSG PCRE_LIBRARY PCRE_INCLUDE_DIR)
 
 if (PCRE_FOUND)
     set( PCRE_LIBRARIES ${PCRE_LIBRARY} )
@@ -28,4 +28,4 @@ else (PCRE_FOUND)
     set( PCRE_INCLUDE_DIRS )
 endif (PCRE_FOUND)
 
-MARK_AS_ADVANCED( PCRE_LIBRARIES PCRE_INCLUDE_DIRS )
+mark_as_advanced( PCRE_LIBRARIES PCRE_INCLUDE_DIRS )
