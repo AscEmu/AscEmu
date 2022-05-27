724ac2282f3c5a45e1f5ffb0e8c5010e98c818eb
aaron02 <aaron.herrsche@hotmail.com>
2022-05-27 15:47:08 +0200

    Update GameObject.cpp

--- src/world/Objects/GameObject.cpp
+++ src/world/Objects/GameObject.cpp
@@ -319,13 +319,13 @@ bool GameObject::loadFromDB(uint32_t spawnId, WorldMap* map, bool addToWorld)
         else
         {
             m_respawnDelayTime = data->spawntimesecs;
-            m_respawnTime = getWorldMap()->getGORespawnTime(data->id);
+            m_respawnTime = map->getGORespawnTime(data->id);
 
             // ready to respawn
             if (m_respawnTime && m_respawnTime <= Util::getTimeNow())
             {
                 m_respawnTime = 0;
-                getWorldMap()->removeRespawnTime(SPAWN_TYPE_GAMEOBJECT, data->id);
+                map->removeRespawnTime(SPAWN_TYPE_GAMEOBJECT, data->id);
             }
         }
     }
