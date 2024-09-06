775318f04c2281ca1bbb4299afbaef508df4dd8d
Zyres <mr@eichbuehlerhof.de>
2024-09-06 22:29:54 +0200

    Missing includes for *nix

--- src/shared/Utilities/Narrow.cpp
+++ src/shared/Utilities/Narrow.cpp
@@ -4,6 +4,7 @@ This file is released under the MIT license. See README-MIT for more information
 */
 
 #include "Utilities/Narrow.hpp"
+#include <cmath>
 
 namespace Util
 {
--- src/shared/Utilities/Narrow.hpp
+++ src/shared/Utilities/Narrow.hpp
@@ -7,6 +7,7 @@ This file is released under the MIT license. See README-MIT for more information
 
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 namespace Util
 {
