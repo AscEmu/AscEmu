f91c7e278f259f22e3acabedb79f51019e13b55a
Zyres <mr@eichbuehlerhof.de>
2025-09-07 14:25:15 +0200

    Misc include

--- src/world/Movement/Spline/SplineChain.h
+++ src/world/Movement/Spline/SplineChain.h
@@ -6,6 +6,7 @@ This file is released under the MIT license. See README-MIT for more information
 #pragma once
 
 #include "MoveSplineInitArgs.h"
+#include <cstdint>
 
 struct SERVER_DECL SplineChainLink
 {
