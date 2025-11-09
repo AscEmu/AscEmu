9babf028c582b4a8e200031e45d35b9327622115
schnek <tolstikov.misha@gmail.com>
2025-11-09 19:27:33 +0300

    fix + fix local (what)
    DELETE FROM `creature_spawns` WHERE `id`=290102;
    DELETE FROM `creature_spawns` WHERE `id`=285746;
    DELETE FROM `creature_spawns` WHERE `id`=288326;

--- sql/world/updates/20251108-00_creature_spawns.sql
+++ sql/world/updates/20251108-00_creature_spawns.sql
@@ -1,10 +1,5 @@
 -- double spawn
-DELETE FROM `creature_spawns` WHERE `id`=5403;
-DELETE FROM `creature_spawns` WHERE `id`=20663;
-DELETE FROM `creature_spawns` WHERE `id`=34043;
 DELETE FROM `creature_spawns` WHERE `id`=45483;
-DELETE FROM `creature_spawns` WHERE `id`=51331;
-DELETE FROM `creature_spawns` WHERE `id`=64730;
 DELETE FROM `creature_spawns` WHERE `id`=122484;
 DELETE FROM `creature_spawns` WHERE `id`=134171;
 DELETE FROM `creature_spawns` WHERE `id`=134729;
@@ -20,15 +15,21 @@ DELETE FROM `creature_spawns` WHERE `id`=288327;
 DELETE FROM `creature_spawns` WHERE `id`=288328;
 DELETE FROM `creature_spawns` WHERE `id`=289416;
 DELETE FROM `creature_spawns` WHERE `id`=289642;
+DELETE FROM `creature_spawns` WHERE `id`=290103;
 DELETE FROM `creature_spawns` WHERE `id`=298161;
 DELETE FROM `creature_spawns` WHERE `id`=313793;
 DELETE FROM `creature_spawns` WHERE `id`=313892;
 DELETE FROM `creature_spawns` WHERE `id`=313903;
+DELETE FROM `creature_spawns` WHERE `id`=316446;
+
+DELETE FROM `creature_spawns` WHERE `id`=290102;
+DELETE FROM `creature_spawns` WHERE `id`=285746;
+DELETE FROM `creature_spawns` WHERE `id`=288326;
 
 -- fix Anduin Wrynn
 DELETE FROM `creature_spawns` WHERE `id`=58983 AND `min_build`=12340;
 UPDATE `creature_spawns` SET `id`=58983, `position_x`='-8440.71', `position_y`='332.994', `position_z`='123.57928', `orientation`='2.25725' WHERE  `id`=58982 AND `min_build`=12340;
 UPDATE `creature_spawns` SET `orientation`=6.280503 WHERE  `id`=57799 AND `min_build`=12340;
 
-INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES ('156', '20251108-00_creature_spawns');
+INSERT INTO `world_db_version` (`id`, `LastUpdate`) VALUES ('156', '20251109-00_creature_spawns');
 
