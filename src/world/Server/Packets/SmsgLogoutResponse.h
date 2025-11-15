53e3650c7589ea48367c9682d59dbea383201e9e
schnek <tolstikov.misha@gmail.com>
2025-11-15 21:29:42 +0300

    Packet: fix SmsgLogoutResponse (Vanilla support)

--- src/world/Server/Packets/SmsgLogoutResponse.h
+++ src/world/Server/Packets/SmsgLogoutResponse.h
@@ -17,31 +17,47 @@ namespace AscEmu::Packets
         bool logout_denied;
 
         uint32_t fail_reason;
-        uint8_t unk2;
+        uint8_t result;
 
         SmsgLogoutResponse() : SmsgLogoutResponse(false)
         {
         }
 
+#if VERSION_STRING == Classic
+        SmsgLogoutResponse(uint8_t result) :
+            ManagedPacket(SMSG_LOGOUT_RESPONSE, 1),
+            result(result)
+#else
         SmsgLogoutResponse(bool logout_denied) :
             ManagedPacket(SMSG_LOGOUT_RESPONSE, 5),
             logout_denied(logout_denied),
             fail_reason(0),
-            unk2(0)
+            result(0)
+#endif
         {
         }
 
     protected:
         bool internalSerialise(WorldPacket& packet) override
         {
+#if VERSION_STRING == Classic
+            packet << result;
+            return true;
+#else
             packet << (logout_denied ? uint32_t(1) : uint32_t(0)) << uint8_t(0);
             return true;
+#endif
         }
 
         bool internalDeserialise(WorldPacket& packet) override
         {
-            packet >> fail_reason >> unk2;
+#if VERSION_STRING == Classic
+            packet >> result;
             return true;
+#else
+            packet >> fail_reason >> result;
+            return true;
+#endif
         }
     };
 }
