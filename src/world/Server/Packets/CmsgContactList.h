274a15d2a1f550b710720938a00bb855add7e424
schnek <tolstikov.misha@gmail.com>
2026-03-29 12:13:11 +0300

    Update CmsgContactList

--- src/world/Server/Packets/CmsgContactList.h
+++ src/world/Server/Packets/CmsgContactList.h
@@ -15,6 +15,11 @@ namespace AscEmu::Packets
     class CmsgContactList : public ManagedPacket
     {
     public:
+#if VERSION_STRING < Mop
+        static constexpr uint16_t packetSize = 4;
+#else
+        static constexpr uint16_t packetSize = 5;
+#endif
         uint32_t list_flag;
 
         CmsgContactList() : CmsgContactList(0)
@@ -22,7 +27,7 @@ namespace AscEmu::Packets
         }
 
         CmsgContactList(uint32_t list_flag) :
-            ManagedPacket(CMSG_CONTACT_LIST, 4),
+            ManagedPacket(CMSG_CONTACT_LIST, packetSize),
             list_flag(list_flag)
         {
         }
@@ -31,12 +36,18 @@ namespace AscEmu::Packets
         bool internalSerialise(WorldPacket& packet) override
         {
             packet << list_flag;
+#if VERSION_STRING >= Mop
+            packet << uint8_t(0);
+#endif
             return true;
         }
 
         bool internalDeserialise(WorldPacket& packet) override
         {
             packet >> list_flag;
+#if VERSION_STRING >= Mop
+            packet.read_skip<uint8_t>();
+#endif
             return true;
         }
     };
