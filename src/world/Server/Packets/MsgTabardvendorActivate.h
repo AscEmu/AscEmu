104d9db12d05c59c52140b65be36143c632b23fa
Zyres <mr@eichbuehlerhof.de>
2026-08-03 18:37:18 +0200

    Enable/Implement MSG_TABARDVENDOR_ACTIVATE (Mop)

--- src/world/Server/Packets/MsgTabardvendorActivate.h
+++ src/world/Server/Packets/MsgTabardvendorActivate.h
@@ -20,7 +20,7 @@ namespace AscEmu::Packets
         }
 
         MsgTabardvendorActivate(uint64_t guid) :
-            ManagedPacket(MSG_TABARDVENDOR_ACTIVATE, 8),
+            ManagedPacket(MSG_TABARDVENDOR_ACTIVATE, 0),
             guid(guid)
         {
         }
@@ -30,16 +30,45 @@ namespace AscEmu::Packets
 
         bool internalSerialise(WorldPacket& packet) override
         {
-            packet << guid.getRawGuid();
+            if (m_protocol.expansion < WoW::Expansion::_Mop)
+            {
+                packet << guid.getRawGuid();
+            }
+            else // Mop
+            {
+                packet.writeBit(guid[1]);
+                packet.writeBit(guid[5]);
+                packet.writeBit(guid[0]);
+                packet.writeBit(guid[7]);
+                packet.writeBit(guid[4]);
+                packet.writeBit(guid[6]);
+                packet.writeBit(guid[3]);
+                packet.writeBit(guid[2]);
+
+                packet.writeByteSeq(guid[5]);
+                packet.writeByteSeq(guid[4]);
+                packet.writeByteSeq(guid[2]);
+                packet.writeByteSeq(guid[3]);
+                packet.writeByteSeq(guid[6]);
+                packet.writeByteSeq(guid[0]);
+                packet.writeByteSeq(guid[1]);
+                packet.writeByteSeq(guid[7]);
+            }
+
             return true;
         }
 
         bool internalDeserialise(WorldPacket& packet) override
         {
-            uint64_t unpackedGuid;
-            packet >> unpackedGuid;
-            guid.init(unpackedGuid);
-            return true;
+            if (m_protocol.expansion < WoW::Expansion::_Mop)
+            {
+                uint64_t unpackedGuid;
+                packet >> unpackedGuid;
+                guid.init(unpackedGuid);
+                return true;
+            }
+
+            return false;
         }
     };
 }
