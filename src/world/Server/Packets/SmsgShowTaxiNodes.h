71c15f1f6091902e5cbcec331257ac7966c740fc
Zyres <mr@eichbuehlerhof.de>
2026-08-23 17:16:14 +0200

    Update SMSG_SHOWTAXINODES for Mop
    tested on Mop - it works

--- src/world/Server/Packets/SmsgShowTaxiNodes.h
+++ src/world/Server/Packets/SmsgShowTaxiNodes.h
@@ -38,8 +38,44 @@ size_t expectedSize() const override
 
         bool internalSerialise(WorldPacket& packet) override
         {
+            if (m_protocol.isMop())
+            {
+                const WoWGuid npcGuid(guid);
+
+                packet.writeBit(true); // unk
+
+                packet.writeBit(npcGuid[3]);
+                packet.writeBit(npcGuid[0]);
+                packet.writeBit(npcGuid[4]);
+                packet.writeBit(npcGuid[2]);
+                packet.writeBit(npcGuid[1]);
+                packet.writeBit(npcGuid[7]);
+                packet.writeBit(npcGuid[6]);
+                packet.writeBit(npcGuid[5]);
+
+                packet.writeBits(DBC_TAXI_MASK_SIZE, 24);
+                packet.flushBits();
+
+                packet.writeByteSeq(npcGuid[0]);
+                packet.writeByteSeq(npcGuid[3]);
+
+                packet << nearestNode;
+
+                packet.writeByteSeq(npcGuid[5]);
+                packet.writeByteSeq(npcGuid[2]);
+                packet.writeByteSeq(npcGuid[6]);
+                packet.writeByteSeq(npcGuid[1]);
+                packet.writeByteSeq(npcGuid[7]);
+                packet.writeByteSeq(npcGuid[4]);
+
+                for (auto tMask : taxiMask)
+                    packet << static_cast<uint8_t>(tMask);
+
+                return true;
+            }
+
             packet << uint32_t(1) << guid << nearestNode;
-            if (m_protocol.expansion >= WoW::Expansion::_Cata)
+            if (m_protocol.isCata())
             {
                 packet << uint32_t(DBC_TAXI_MASK_SIZE);
             }
